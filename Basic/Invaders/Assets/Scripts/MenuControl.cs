fc239bcff773c8a4bbd2d432985c2a029cac481b
Cosmin <cosmin.bararu@unity3d.com>
2021-03-05 15:06:52 +0000

    [Invaders] Minor fixes after update to release/0.1.0 & add gitignore file

--- Basic/Invaders/Assets/Scripts/MenuControl.cs
+++ Basic/Invaders/Assets/Scripts/MenuControl.cs
@@ -15,7 +15,7 @@ public class MenuControl : MonoBehaviour
     public void StartLocalGame()
     {
         // Update the current HostNameInput with whatever we have set in the NetworkConfig as default
-        var unetTransport = (UnetTransport)NetworkManager.Singleton.NetworkConfig.NetworkTransport;
+        var unetTransport = (UNetTransport)NetworkManager.Singleton.NetworkConfig.NetworkTransport;
         if (unetTransport) m_HostIpInput.text = unetTransport.ConnectAddress;
         LobbyControl.isHosting = true; //This is a work around to handle proper instantiation of a scene for the first time.(See LobbyControl.cs)
         SceneTransitionHandler.sceneTransitionHandler.SwitchScene(m_LobbySceneName);
@@ -25,7 +25,7 @@ public class MenuControl : MonoBehaviour
     {
         if (m_HostIpInput.text != "Hostname")
         {
-            var unetTransport = (UnetTransport)NetworkManager.Singleton.NetworkConfig.NetworkTransport;
+            var unetTransport = (UNetTransport)NetworkManager.Singleton.NetworkConfig.NetworkTransport;
             if (unetTransport) unetTransport.ConnectAddress = m_HostIpInput.text;
             LobbyControl.isHosting = false; //This is a work around to handle proper instantiation of a scene for the first time.  (See LobbyControl.cs)
             SceneTransitionHandler.sceneTransitionHandler.SwitchScene(m_LobbySceneName);
