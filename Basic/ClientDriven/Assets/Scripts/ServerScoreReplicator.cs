07d05f2fed3e04cff85441edb6c8e95bcfc41932
Samuel Bellomo <samuel.bellomo@unity3d.com>
2021-10-13 22:03:52 -0400

    removing useless code

--- Basic/ClientDriven/Assets/Scripts/ServerScoreReplicator.cs
+++ Basic/ClientDriven/Assets/Scripts/ServerScoreReplicator.cs
@@ -3,22 +3,11 @@ using Unity.Netcode.Samples;
 
 public class ServerScoreReplicator : ClientServerBaseNetworkBehaviour
 {
-    // [SerializeField]
-    // private ScoreTracker m_Score;
-
     private NetworkVariable<int> m_ReplicatedScore = new NetworkVariable<int>();
 
     public int Score
     {
-        get { return m_ReplicatedScore.Value;}
-        set { m_ReplicatedScore.Value = value; }
-    }
-
-    public override void OnNetworkSpawn()
-    {
-        base.OnNetworkSpawn();
-        if (!IsServer) return;
-        // m_Score.replicator = m_ReplicatedScore;
-        // m_Score.Score = 0;
+        get => m_ReplicatedScore.Value;
+        set => m_ReplicatedScore.Value = value;
     }
 }
