c4576a7b6172ffd849fc5a159ac262918aece56e
Luke Stampfli <luke@stampfli.one>
2021-10-15 10:56:14 +0100

    Update to latest NGO

--- Basic/2DSpaceShooter/Assets/Scripts/NetworkObjectPool.cs
+++ Basic/2DSpaceShooter/Assets/Scripts/NetworkObjectPool.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using MLAPI;
-using MLAPI.Spawning;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Assertions;
 
--- Basic/2DSpaceShooter/Assets/Scripts/RandomPositionPlayerSpawner.cs
+++ Basic/2DSpaceShooter/Assets/Scripts/RandomPositionPlayerSpawner.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using MLAPI;
+using Unity.Netcode;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
