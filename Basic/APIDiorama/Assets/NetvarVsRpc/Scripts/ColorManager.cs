b8f0c697d7acd7ecf8771536a64c858636264dca
Paolo Abela <rikudefuffs@gmail.com>
2022-11-18 17:20:37 +0100

    [Feature] Added a script to manage and change the color of a ColoredCube using NetworkVariables and RPCs [Project] Added ColoredCube prefab [Project] Grouped assets specific to this scene under a specific folder

---
+++ Basic/APIDiorama/Assets/NetvarVsRpc/Scripts/ColorManager.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+namespace Unity.Netcode.Samples.APIDiorama
+{
+    public class ColorManager : NetworkBehaviour
+    {
+        [SerializeField]
+        bool m_UseNetworkVariableForColor;
+
+        NetworkVariable<Color32> m_NetworkedColor = new NetworkVariable<Color32>();
+        Material m_Material;
+
+        void Awake()
+        {
+            m_Material = GetComponent<Renderer>().material;
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            if (IsClient)
+            {
+                if (m_UseNetworkVariableForColor)
+                {
+                    OnClientColorChanged(m_Material.color, m_NetworkedColor.Value);
+                    m_NetworkedColor.OnValueChanged += OnClientColorChanged;
+                }
+            }
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            base.OnNetworkDespawn();
+            if (IsClient)
+            {
+                if (m_UseNetworkVariableForColor)
+                {
+                    m_NetworkedColor.OnValueChanged -= OnClientColorChanged;
+                }
+            }
+        }
+
+        void Update()
+        {
+            if (!IsClient)
+            {
+                return;
+            }
+
+            bool playerIsCloseEnough = true; //todo: implement behaviour
+            if (playerIsCloseEnough)
+            {
+                if (Input.GetKeyUp(KeyCode.E))
+                {
+                    OnClientRequestColorChange();
+                }
+            }
+        }
+
+        void OnClientRequestColorChange()
+        {
+            OnServerChangeColorServerRpc();
+        }
+
+        [ServerRpc]
+        void OnServerChangeColorServerRpc()
+        {
+            Color32 newColor = GetRandomColor();
+            if (m_UseNetworkVariableForColor)
+            {
+                m_NetworkedColor.Value = newColor;
+                return;
+            }
+            OnClientNotifyColorChangedClientRpc(newColor);
+        }
+
+        [ClientRpc]
+        void OnClientNotifyColorChangedClientRpc(Color32 newColor)
+        {
+            m_Material.color = newColor;
+        }
+
+        void OnClientColorChanged(Color32 previousColor, Color32 newColor)
+        {
+            m_Material.color = newColor;
+        }
+
+        Color32 GetRandomColor() => new Color32((byte)Random.Range(0, 256), (byte)Random.Range(0, 256), (byte)Random.Range(0, 256), 255);
+    }
+}
