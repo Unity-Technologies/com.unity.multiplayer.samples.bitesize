24303bdfd6cf675958fcd0d03d5be65cbb1ed1b5
Paolo Abela <rikudefuffs@gmail.com>
2022-11-23 12:12:24 +0100

    [UI] (API Diorama) The color of the button representing the selected network mode (Host/Client/Server) changes when a network mode is selected

--- Basic/APIDiorama/Assets/Scripts/NetworkManagerUI.cs
+++ Basic/APIDiorama/Assets/Scripts/NetworkManagerUI.cs
@@ -1,34 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour
 {
-    [SerializeField] Button btnServer;
-    [SerializeField] Button btnHost;
-    [SerializeField] Button btnClient;
+    [SerializeField] Button m_ServerButton;
+    [SerializeField] Button m_HostButton;
+    [SerializeField] Button m_ClientButton;
+    [SerializeField] Color32 m_HighlightedButtonColor;
+    [SerializeField] Color32 m_NormalButtonColor;
+    Button[] m_Buttons;
+
 
     void Awake()
     {
-        btnServer.onClick.AddListener(StartServer);
-        btnHost.onClick.AddListener(StartHost);
-        btnClient.onClick.AddListener(StartClient);
+        m_ServerButton.onClick.AddListener(StartServer);
+        m_HostButton.onClick.AddListener(StartHost);
+        m_ClientButton.onClick.AddListener(StartClient);
+        m_Buttons = new Button[] { m_ServerButton, m_HostButton, m_ClientButton };
     }
 
     void StartServer()
     {
         NetworkManager.Singleton.StartServer();
+        HighlightButton(m_ServerButton);
     }
 
     void StartHost()
     {
         NetworkManager.Singleton.StartHost();
+        HighlightButton(m_HostButton);
     }
 
     void StartClient()
     {
         NetworkManager.Singleton.StartClient();
+        HighlightButton(m_ClientButton);
+    }
+
+    void HighlightButton(Button buttonToHighlight)
+    {
+        foreach (var button in m_Buttons)
+        {
+            ColorBlock colors = button.colors;
+            colors.normalColor = button == buttonToHighlight ? m_HighlightedButtonColor
+                                                             : m_NormalButtonColor;
+            button.colors = colors;
+        }
     }
 }
