752a067502f02bd8eb3cf7a0883e20345da471e6
Fernando Cortez <fernando.cortez@unity3d.com>
2022-11-30 12:07:10 -0500

    re-enabling footstep sound on ghost clients

--- Basic/ClientDriven/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Basic/ClientDriven/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -377,35 +377,25 @@ namespace StarterAssets
 
         private void OnFootstep(AnimationEvent animationEvent)
         {
-            // MTT CHANGE START
-            if (!enabled)
-            {
-                return;
-            }
-            // MTT CHANGE END
-            
             if (animationEvent.animatorClipInfo.weight > 0.5f)
             {
                 if (FootstepAudioClips.Length > 0)
                 {
                     var index = Random.Range(0, FootstepAudioClips.Length);
-                    AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(_controller.center), FootstepAudioVolume);
+                    // MTT CHANGE START
+                    AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.position, FootstepAudioVolume);
+                    // MTT CHANGE END
                 }
             }
         }
 
         private void OnLand(AnimationEvent animationEvent)
         {
-            // MTT CHANGE START
-            if (!enabled)
-            {
-                return;
-            }
-            // MTT CHANGE END
-            
             if (animationEvent.animatorClipInfo.weight > 0.5f)
             {
-                AudioSource.PlayClipAtPoint(LandingAudioClip, transform.TransformPoint(_controller.center), FootstepAudioVolume);
+                // MTT CHANGE START
+                AudioSource.PlayClipAtPoint(LandingAudioClip, transform.position, FootstepAudioVolume);
+                // MTT CHANGE END
             }
         }
     }
