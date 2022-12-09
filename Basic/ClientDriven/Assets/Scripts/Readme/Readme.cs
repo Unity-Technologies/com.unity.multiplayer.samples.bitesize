4ab59e54af38f17c86ba5234f6a3efb2dee0da36
Fernando Cortez <fernando.cortez@unity3d.com>
2022-12-09 11:07:45 -0500

    feat: ClientDriven readme (#76)
    * imported readme, removed starter assets readme files, readme wip
    
    * readme cleanup
    
    * comment feedback
    
    * Updated Readme to have a fancy client driven image
    
    Co-authored-by: jilfranco-unity <jillian.franco@unity3d.com>

---
+++ Basic/ClientDriven/Assets/Scripts/Readme/Readme.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+/// <remarks>
+/// Custom readme class based on the readme created for URP. For more context, see:
+/// https://github.com/Unity-Technologies/Graphics/tree/master/com.unity.template-universal
+/// </remarks>
+[CreateAssetMenu]
+public class Readme : ScriptableObject
+{
+    public Texture2D icon;
+    public string title;
+    public Section[] sections;
+    public bool loadedLayout;
+
+    [Serializable]
+    public class Section
+    {
+        public string heading;
+        public string text;
+        public string linkText;
+        public string url;
+    }
+}
