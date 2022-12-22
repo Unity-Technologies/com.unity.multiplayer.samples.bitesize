a5cfb99a133907880f3fa11cfd8a312f0da8539e
Paolo Abela <rikudefuffs@gmail.com>
2022-12-22 12:54:29 +0100

    [Project] Removed custom implementation fo ClientNetworkAnimator to use the one from BossRoom instead

--- Basic/APIDiorama/Assets/Scripts/ClientNetworkAnimator.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Netcode.Components;
-
-public class ClientNetworkAnimator : NetworkAnimator
-{
-    protected override bool OnIsServerAuthoritative()
-    {
-        return false; //allows the client to send animations data over the network
-    }
-}
