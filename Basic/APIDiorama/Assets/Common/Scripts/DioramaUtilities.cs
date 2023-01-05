4b0e26051481ef1eca38196cefdf03c42126956b
Paolo Abela <rikudefuffs@gmail.com>
2023-01-05 11:35:45 +0100

    [Feature] Added custom data types synchronization example to the NetworkVariables example scene

--- Basic/APIDiorama/Assets/Common/Scripts/DioramaUtilities.cs
+++ Basic/APIDiorama/Assets/Common/Scripts/DioramaUtilities.cs
@@ -7,10 +7,13 @@ namespace Unity.Netcode.Samples.APIDiorama
     /// </summary>
     public static class DioramaUtilities
     {
+        static readonly string[] s_Usernames = new string[] { "MorwennaDaBest", "SamTheBell", "FranklyJil", "FernandoCutter", "LP Morgan" };
+
         /// <summary>
         /// Generates a random color
         /// </summary>
         /// <returns>A random RGBA color</returns>
         public static Color32 GetRandomColor() => new Color32((byte)Random.Range(0, 256), (byte)Random.Range(0, 256), (byte)Random.Range(0, 256), 255);
+        public static string GetRandomUsername() => s_Usernames[Random.Range(0, s_Usernames.Length)];
     }
 }
