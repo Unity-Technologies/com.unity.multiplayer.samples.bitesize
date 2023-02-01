bc042ec3b6d80e0a2693411d888a324c63ed79d8
Paolo Abela <rikudefuffs@gmail.com>
2023-02-01 13:12:39 +0100

    [UI] Added a "Quit scene" button to go back to the Selection screen from any scene while playing

--- Basic/APIDiorama/Assets/Common/Scripts/NetworkManagerUI.cs
+++ Basic/APIDiorama/Assets/Common/Scripts/NetworkManagerUI.cs
@@ -1,81 +1,96 @@
+using DevLocker.Utils;
+using System;
 using System.Linq;
-using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class NetworkManagerUI : MonoBehaviour
+namespace Unity.Netcode.Samples.APIDiorama
 {
-    [SerializeField] Button m_ServerButton;
-    [SerializeField] Button m_HostButton;
-    [SerializeField] Button m_ClientButton;
-    [SerializeField] Button m_DisconnectButton;
-    [SerializeField] Color32 m_HighlightedButtonColor;
-    [SerializeField] Color32 m_DisabledButtonColor;
-    Button[] m_Buttons;
 
-    void Awake()
+    public class NetworkManagerUI : MonoBehaviour
     {
-        m_ServerButton.onClick.AddListener(StartServer);
-        m_HostButton.onClick.AddListener(StartHost);
-        m_ClientButton.onClick.AddListener(StartClient);
-        m_DisconnectButton.onClick.AddListener(Disconnect);
-        m_Buttons = new Button[] { m_ServerButton, m_HostButton, m_ClientButton };
-    }
+        [SerializeField] Button m_ServerButton;
+        [SerializeField] Button m_HostButton;
+        [SerializeField] Button m_ClientButton;
+        [SerializeField] Button m_DisconnectButton;
+        [SerializeField] Button m_QuitSceneButton;
+        [SerializeField] Color32 m_HighlightedButtonColor;
+        [SerializeField] Color32 m_DisabledButtonColor;
+        [SerializeField] SceneReference m_SelectionScreenScene;
+        Button[] m_Buttons;
 
-    void Start()
-    {
-        SetButtonStateAndColor(m_DisconnectButton, false, false);
-    }
-
-    void StartServer()
-    {
-        NetworkManager.Singleton.StartServer();
-        EnableAndHighlightButtons(m_ServerButton, false);
-        SetButtonStateAndColor(m_DisconnectButton, false, true);
-    }
-
-    void StartHost()
-    {
-        NetworkManager.Singleton.StartHost();
-        EnableAndHighlightButtons(m_HostButton, false);
-        SetButtonStateAndColor(m_DisconnectButton, false, true);
-    }
-
-    void StartClient()
-    {
-        NetworkManager.Singleton.StartClient();
-        EnableAndHighlightButtons(m_ClientButton, false);
-        SetButtonStateAndColor(m_DisconnectButton, false, true);
-    }
-
-    void Disconnect()
-    {
-        if (NetworkManager.Singleton.IsServer)
+        void Awake()
         {
-            foreach (var item in NetworkManager.Singleton.SpawnManager.SpawnedObjectsList.ToList())
+            m_ServerButton.onClick.AddListener(StartServer);
+            m_HostButton.onClick.AddListener(StartHost);
+            m_ClientButton.onClick.AddListener(StartClient);
+            m_DisconnectButton.onClick.AddListener(Disconnect);
+            m_QuitSceneButton.onClick.AddListener(QuitScene);
+            m_Buttons = new Button[] { m_ServerButton, m_HostButton, m_ClientButton };
+        }
+
+        void Start()
+        {
+            SetButtonStateAndColor(m_DisconnectButton, false, false);
+        }
+
+        void StartServer()
+        {
+            NetworkManager.Singleton.StartServer();
+            EnableAndHighlightButtons(m_ServerButton, false);
+            SetButtonStateAndColor(m_DisconnectButton, false, true);
+        }
+
+        void StartHost()
+        {
+            NetworkManager.Singleton.StartHost();
+            EnableAndHighlightButtons(m_HostButton, false);
+            SetButtonStateAndColor(m_DisconnectButton, false, true);
+        }
+
+        void StartClient()
+        {
+            NetworkManager.Singleton.StartClient();
+            EnableAndHighlightButtons(m_ClientButton, false);
+            SetButtonStateAndColor(m_DisconnectButton, false, true);
+        }
+
+        void Disconnect()
+        {
+            if (NetworkManager.Singleton.IsServer)
             {
-                item.Despawn(false);
+                foreach (var item in NetworkManager.Singleton.SpawnManager.SpawnedObjectsList.ToList())
+                {
+                    item.Despawn(false);
+                }
+            }
+            NetworkManager.Singleton.Shutdown();
+            EnableAndHighlightButtons(null, true);
+            SetButtonStateAndColor(m_DisconnectButton, false, false);
+        }
+
+        void QuitScene()
+        {
+            Disconnect();
+            SceneManager.LoadScene(m_SelectionScreenScene.SceneName, LoadSceneMode.Single);
+        }
+
+        void EnableAndHighlightButtons(Button buttonToHighlight, bool enable)
+        {
+            foreach (var button in m_Buttons)
+            {
+                SetButtonStateAndColor(button, button == buttonToHighlight, enable);
             }
         }
-        NetworkManager.Singleton.Shutdown();
-        EnableAndHighlightButtons(null, true);
-        SetButtonStateAndColor(m_DisconnectButton, false, false);
-    }
 
-    void EnableAndHighlightButtons(Button buttonToHighlight, bool enable)
-    {
-        foreach (var button in m_Buttons)
+        void SetButtonStateAndColor(Button button, bool highlight, bool enable)
         {
-            SetButtonStateAndColor(button, button == buttonToHighlight, enable);
+            ColorBlock colors = button.colors;
+            colors.disabledColor = highlight ? m_HighlightedButtonColor
+                                             : m_DisabledButtonColor;
+            button.colors = colors;
+            button.interactable = enable;
         }
-    }
-
-    void SetButtonStateAndColor(Button button, bool highlight, bool enable)
-    {
-        ColorBlock colors = button.colors;
-        colors.disabledColor = highlight ? m_HighlightedButtonColor
-                                         : m_DisabledButtonColor;
-        button.colors = colors;
-        button.interactable = enable;
     }
 }
