ceb7afb2f421cc4b9ccdc60f12fcdd92e7ae9a80
Fernando Cortez <fernando.cortez@unity3d.com>
2023-02-16 19:34:59 -0500

    fixed blocking yamato issue (implementation of name fetching that works outside editor)

--- Basic/DynamicAddressablesNetworkPrefabs/Assets/Scripts/02_Server Authoritative Load All Async/ServerAuthoritativeLoadAllAsync.cs
+++ Basic/DynamicAddressablesNetworkPrefabs/Assets/Scripts/02_Server Authoritative Load All Async/ServerAuthoritativeLoadAllAsync.cs
@@ -172,14 +172,19 @@ namespace Game.ServerAuthoritativeLoadAllAsync
                 Debug.Log("Loading dynamic prefab on the clients...");
                 LoadAddressableClientRpc(assetGuid);
                 
-                // server is starting to load a prefab, update UI
-                m_InGameUI.ClientLoadedPrefabStatusChanged(NetworkManager.ServerClientId, assetGuid.GetHashCode(), "Undefined", InGameUI.LoadStatus.Loading);
-                
                 await DynamicPrefabLoadingUtilities.LoadDynamicPrefab(assetGuid, m_InGameUI.ArtificialDelayMilliseconds);
                 
                 // server loaded a prefab, update UI with the loaded asset's name
                 DynamicPrefabLoadingUtilities.TryGetLoadedGameObjectFromGuid(assetGuid, out var loadedGameObject);
-                m_InGameUI.ClientLoadedPrefabStatusChanged(NetworkManager.ServerClientId, assetGuid.GetHashCode(), loadedGameObject.Result.name, InGameUI.LoadStatus.Loaded);
+                
+                // every client loaded dynamic prefab, their respective ClientUIs in case they loaded first
+                foreach (var client in m_NetworkManager.ConnectedClients.Keys)
+                {
+                    m_InGameUI.ClientLoadedPrefabStatusChanged(client,
+                        assetGuid.GetHashCode(), 
+                        loadedGameObject.Result.name, 
+                        InGameUI.LoadStatus.Loading);
+                }
             }
         }
         
@@ -214,13 +219,20 @@ namespace Game.ServerAuthoritativeLoadAllAsync
             DynamicPrefabLoadingUtilities.RecordThatClientHasLoadedAPrefab(prefabHash, rpcParams.Receive.SenderClientId);
             
             // a quick way to grab a matching prefab reference's name via its prefabHash
-            var loadedPrefabName = prefabHash.ToString();
+            var loadedPrefabName = "Undefined";
             foreach (var prefabReference in m_DynamicPrefabReferences)
             {
                 var prefabReferenceGuid = new AddressableGUID() { Value = prefabReference.AssetGUID };
                 if (prefabReferenceGuid.GetHashCode() == prefabHash)
                 {
-                    loadedPrefabName = prefabReference.editorAsset.name;
+                    // found the matching prefab reference
+                    if (DynamicPrefabLoadingUtilities.LoadedDynamicPrefabResourceHandles.TryGetValue(
+                            prefabReferenceGuid, 
+                            out var loadedGameObject))
+                    {
+                        // if it is loaded on the server, update the name on the ClientUI
+                        loadedPrefabName = loadedGameObject.Result.name;
+                    }
                     break;
                 }
             }
