6a6fe0efc485c4f4d80fb2500b7e09cdcb705031
Frank Luong <100299641+fluong6@users.noreply.github.com>
2023-07-19 16:38:39 -0400

    refactoring to reduce line of code

--- Basic/ClientDriven/Assets/Scripts/InfoPanel.cs
+++ Basic/ClientDriven/Assets/Scripts/InfoPanel.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 
 public class InfoPanel : MonoBehaviour
 {
-    public List<ToggleEntry> ToggleEntries;
+    public Toggle HalfFloatToggle;
+    public Toggle InterpolateToggle;
+    public Toggle QuatSynchToggle;
+    public Toggle QuatCompToggle;
     private ClientNetworkTransform ClientNetworkTransform;
 
     void Start()
@@ -14,42 +17,28 @@ public class InfoPanel : MonoBehaviour
         LocalPlayer.OnNetworkSpawnEvent += OnClientPlayerNetworkSpawn;
     }
 
-    void Awake()
-    {
-        Debug.Assert(ToggleEntries != null && ToggleEntries.Count > 0);
-    }
-
     void Update()
     {
-        if (!Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        QuatCompToggle.interactable = QuatSynchToggle.isOn;
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            var toggleEntry = ToggleEntries.Find(entry => entry.Toggle.name == "HalfFloat");
-            toggleEntry.Toggle.isOn = !toggleEntry.Toggle.isOn;
+            HalfFloatToggle.isOn = !HalfFloatToggle.isOn;
         } 
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            var toggleEntry = ToggleEntries.Find(entry => entry.Toggle.name == "QuatSynch");
-            toggleEntry.Toggle.isOn = !toggleEntry.Toggle.isOn;
+            InterpolateToggle.isOn = !InterpolateToggle.isOn;
         } 
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            // Only activate Quaternion Compression if Quaternion Synchronization is enabled
-            if (ClientNetworkTransform.UseQuaternionSynchronization)
-            {
-                var toggleEntry = ToggleEntries.Find(entry => entry.Toggle.name == "QuatComp");
-                toggleEntry.Toggle.isOn = !toggleEntry.Toggle.isOn;
-            }
+            QuatSynchToggle.isOn = !QuatSynchToggle.isOn;
         } 
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            var toggleEntry = ToggleEntries.Find(entry => entry.Toggle.name == "Interpolate");
-            toggleEntry.Toggle.isOn = !toggleEntry.Toggle.isOn;
+            QuatCompToggle.isOn = !QuatCompToggle.isOn;
         }
     }
     private void OnClientPlayerNetworkSpawn(ClientNetworkTransform clientNetworkTransform)
@@ -62,67 +51,42 @@ public class InfoPanel : MonoBehaviour
     
     public void OnUpdateHalfFloat(bool isOn)
     {
-        // Question: Do I need to specify that this is a client-side function? (adding IsClient in the if statement)
-        ClientNetworkTransform.UseHalfFloatPrecision = isOn;
+        if (ClientNetworkTransform != null)
+        {
+            ClientNetworkTransform.UseHalfFloatPrecision = isOn;
+        }
     }
 
     public void OnUpdateQuatSync(bool isOn)
     {
-        ClientNetworkTransform.UseQuaternionSynchronization = isOn;
+        if (ClientNetworkTransform != null)
+        {
+            ClientNetworkTransform.UseQuaternionSynchronization = isOn;
+        }
     }
 
     public void OnUpdateQuatComp(bool isOn)
     {
-        ClientNetworkTransform.UseQuaternionCompression = isOn;
+        if (ClientNetworkTransform != null)
+        {
+            ClientNetworkTransform.UseQuaternionCompression = isOn;
+        }
     }
 
     public void OnInterpolate(bool isOn)
     {
-        ClientNetworkTransform.Interpolate = isOn;
+        if (ClientNetworkTransform != null)
+        {
+            ClientNetworkTransform.Interpolate = isOn;
+        }
     }
 
     private void UpdateClientSideToggles()
     {
-        foreach (var entry in ToggleEntries)
-        {
-            switch (entry.ToggleEntryType)
-            {
-                case ToggleEntry.ToggleEntryTypes.HalfFloat:
-                    {
-                        entry.Toggle.isOn = ClientNetworkTransform.UseHalfFloatPrecision;
-                        break;
-                    }
-                case ToggleEntry.ToggleEntryTypes.QuatSynch:
-                    {
-                        entry.Toggle.isOn = ClientNetworkTransform.UseQuaternionSynchronization;
-                        break;
-                    }
-                case ToggleEntry.ToggleEntryTypes.QuatComp:
-                    {
-                        entry.Toggle.isOn = ClientNetworkTransform.UseQuaternionCompression;
-                        break;
-                    }
-                case ToggleEntry.ToggleEntryTypes.Interpolate:
-                    {
-                        entry.Toggle.isOn = ClientNetworkTransform.Interpolate;
-                        break;
-                    }
-            }
-        }
+        if (ClientNetworkTransform == null) return;
+        HalfFloatToggle.isOn = ClientNetworkTransform.UseHalfFloatPrecision;
+        InterpolateToggle.isOn = ClientNetworkTransform.Interpolate;
+        QuatSynchToggle.isOn = ClientNetworkTransform.UseQuaternionSynchronization;
+        QuatCompToggle.isOn = ClientNetworkTransform.UseQuaternionCompression;
     }
-}
-
-[Serializable]
-public class ToggleEntry
-{
-    public enum ToggleEntryTypes
-    {
-        HalfFloat,
-        QuatSynch,
-        QuatComp,
-        Interpolate,
-    }
-
-    public Toggle Toggle;
-    public ToggleEntryTypes ToggleEntryType;
 }
