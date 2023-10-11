deedde638eadebc376177a619696ad8edd65ccde
NoelStephensUnity <noel.stephens@unity3d.com>
2023-10-11 16:55:38 -0500

    fix
    Missed this file

--- Basic/ClientDriven/Assets/Scripts/ServerPlayerMove.cs
+++ Basic/ClientDriven/Assets/Scripts/ServerPlayerMove.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 public class ServerPlayerMove : NetworkBehaviour
 {
     public NetworkVariable<bool> isObjectPickedUp = new NetworkVariable<bool>();
-    
+
     NetworkObject m_PickedUpObject;
 
     [SerializeField]
@@ -27,7 +27,7 @@ public class ServerPlayerMove : NetworkBehaviour
         }
 
         OnServerSpawnPlayer();
-        
+
         base.OnNetworkSpawn();
     }
 
@@ -37,7 +37,7 @@ public class ServerPlayerMove : NetworkBehaviour
         var spawnPoint = ServerPlayerSpawnPoints.Instance.ConsumeNextSpawnPoint();
         var spawnPosition = spawnPoint ? spawnPoint.transform.position : Vector3.zero;
         transform.position = spawnPosition;
-        
+
         // A note specific to owner authority:
         // Side Note:  Specific to Owner Authoritative
         // Setting the position works as and can be set in OnNetworkSpawn server-side unless there is a
@@ -48,9 +48,21 @@ public class ServerPlayerMove : NetworkBehaviour
         // transform after synchronization with the initial position, thus overwriting the synchronized position.
     }
 
+    public override void OnNetworkDespawn()
+    {
+        if (IsServer && IsLocalPlayer && m_PickedUpObject != null)
+        {
+            m_PickedUpObject.TryRemoveParent(m_PickedUpObject.WorldPositionStays());
+            m_PickedUpObject.GetComponent<ServerIngredient>().ingredientDespawned -= IngredientDespawned;
+            m_PickedUpObject = null;
+        }
+
+        base.OnNetworkDespawn();
+    }
+
     [ServerRpc]
     public void PickupObjectServerRpc(ulong objToPickupID)
-    {        
+    {
         NetworkManager.SpawnManager.SpawnedObjects.TryGetValue(objToPickupID, out var objectToPickup);
         if (objectToPickup == null || objectToPickup.transform.parent != null) return; // object already picked up, server authority says no
 
@@ -66,9 +78,13 @@ public class ServerPlayerMove : NetworkBehaviour
     void IngredientDespawned()
     {
         m_PickedUpObject = null;
+        if (NetworkManager.ShutdownInProgress)
+        {
+            return;
+        }
         isObjectPickedUp.Value = false;
     }
-    
+
     [ServerRpc]
     public void DropObjectServerRpc()
     {
