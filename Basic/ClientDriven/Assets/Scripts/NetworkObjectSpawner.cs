7cfcacd1d05c66b522a20391a5c5be5a8fe3bde9
Elfi Kühndorf <elfi.kuhndorf@unity3d.com>
2024-03-15 11:49:48 +0100

    addressed feedback including namepace, OnDestroy method and null check

--- Basic/ClientDriven/Assets/Scripts/NetworkObjectSpawner.cs
+++ Basic/ClientDriven/Assets/Scripts/NetworkObjectSpawner.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 using UnityEngine.Networking;
 using Random = System.Random;
 
-public class NetworkObjectSpawner : NetworkBehaviour
+namespace Unity.Multiplayer.Samples.ClientDriven
 {
-    [SerializeField] NetworkObject prefabReference;
-
-    public void Start()
+    public class NetworkObjectSpawner : MonoBehaviour
     {
-        NetworkManager.Singleton.OnServerStarted += OnServerStartedIngredientSpawn;
-    }
+        [SerializeField] NetworkObject prefabReference;
 
-    void OnServerStartedIngredientSpawn()
-    {
-        Random randomGenerator = new Random();
-        var instantiatedNetworkObject = Instantiate(prefabReference, transform.position, transform.rotation, null);
-        var ingredient = instantiatedNetworkObject.GetComponent<ServerIngredient>();
-        ingredient.NetworkObject.Spawn();
-        ingredient.currentIngredientType.Value = (IngredientType)randomGenerator.Next((int)IngredientType.MAX);
+        public void Start()
+        {
+            if (NetworkManager.Singleton != null)
+            {
+                NetworkManager.Singleton.OnServerStarted += OnServerStartedIngredientSpawn;
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (NetworkManager.Singleton != null)
+            {
+                NetworkManager.Singleton.OnServerStarted -= OnServerStartedIngredientSpawn;
+            }
+        }
+
+        void OnServerStartedIngredientSpawn()
+        {
+            Random randomGenerator = new Random();
+            var instantiatedNetworkObject = Instantiate(prefabReference, transform.position, transform.rotation, null);
+            var ingredient = instantiatedNetworkObject.GetComponent<ServerIngredient>();
+            ingredient.NetworkObject.Spawn();
+            ingredient.currentIngredientType.Value = (IngredientType)randomGenerator.Next((int)IngredientType.MAX);
+        }
     }
 }
