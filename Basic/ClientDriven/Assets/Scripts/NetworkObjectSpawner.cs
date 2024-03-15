07944124d3f86eca3a5858e6e4f73f7b657f06ef
Elfi Kühndorf <elfi.kuhndorf@unity3d.com>
2024-03-15 17:22:59 +0100

    added prefab variants and added summary as well as debug assert function to NetworkObjectSpawner

--- Basic/ClientDriven/Assets/Scripts/NetworkObjectSpawner.cs
+++ Basic/ClientDriven/Assets/Scripts/NetworkObjectSpawner.cs
@@ -6,33 +6,47 @@ using Random = System.Random;
 
 namespace Unity.Multiplayer.Samples.ClientDriven
 {
+    /// <summary>
+    /// Spawn a NetworkObject at this transform's position when NetworkManer's server is started.
+    /// </summary>
+    /// <remarks>
+    /// A NetworkManager is expected to be part of the scene that this NetworkObject is a part of.
+    /// </remarks>
     public class NetworkObjectSpawner : MonoBehaviour
     {
-        [SerializeField] NetworkObject prefabReference;
+        [SerializeField]
+        private NetworkObject prefabReference;
+        [SerializeField]
+        private NetworkManager m_NetworkManager;
 
         void Start()
         {
-            if (NetworkManager.Singleton != null)
+            Debug.Assert(m_NetworkManager != null, "The NetworkManager needs to be referenced!");
+            if (m_NetworkManager == null)
             {
-                NetworkManager.Singleton.OnServerStarted += OnServerStartedIngredientSpawn;
+                return;
             }
+
+            m_NetworkManager.OnServerStarted += OnServerStartedIngredientSpawn;
         }
 
         void OnDestroy()
         {
-            if (NetworkManager.Singleton != null)
+            Debug.Assert(m_NetworkManager != null, "The NetworkManager needs to be referenced!");
+            if (m_NetworkManager == null)
             {
-                NetworkManager.Singleton.OnServerStarted -= OnServerStartedIngredientSpawn;
+                return;
             }
+
+            m_NetworkManager.OnServerStarted -= OnServerStartedIngredientSpawn;
         }
 
         void OnServerStartedIngredientSpawn()
         {
-            Random randomGenerator = new Random(); 
+            Random randomGenerator = new Random();
             NetworkObject instantiatedNetworkObject = Instantiate(prefabReference, transform.position, transform.rotation, null);
             ServerIngredient ingredient = instantiatedNetworkObject.GetComponent<ServerIngredient>();
             ingredient.NetworkObject.Spawn();
-            ingredient.currentIngredientType.Value = (IngredientType)randomGenerator.Next((int)IngredientType.MAX);
         }
     }
 }
