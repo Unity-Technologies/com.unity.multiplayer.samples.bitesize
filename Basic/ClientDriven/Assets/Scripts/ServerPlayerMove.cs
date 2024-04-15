5476fcf617e97a5850853218480732871f122021
Fernando Cortez <fernando.cortez@unity3d.com>
2024-04-15 18:22:40 -0400

    more context on pattern change

--- Basic/ClientDriven/Assets/Scripts/ServerPlayerMove.cs
+++ Basic/ClientDriven/Assets/Scripts/ServerPlayerMove.cs
@@ -34,12 +34,21 @@ public class ServerPlayerMove : NetworkBehaviour
 
     void OnServerSpawnPlayer()
     {
+        // Note: specific to Netcode for GameObjects v1.8.0 & v1.8.1
+        // Previous versions of this sample utilizing Netcode for GameObjects <v1.8.0 had set the player's position at
+        // this point. However, a regression with these two particular versions forced the new pattern for modifying a
+        // player's initial position, which is:
+        // we store the server-determined spawn position inside of a NetworkVariable, and have that be consumed by the
+        // owning client, inside ClientDrivenNetworkTransform. This approach navigates potential OnNetworkSpawn race
+        // conditions that popped up in these two Netcode versions, and is now a recommended approach for setting spawn
+        // positions on OnNetworkSpawn for owner-authoritative NetworkTransforms.
+
         // this is done server side, so we have a single source of truth for our spawn point list
         var spawnPoint = ServerPlayerSpawnPoints.Instance.ConsumeNextSpawnPoint();
         spawnPosition.Value = spawnPoint != null ? spawnPoint.transform.position : Vector3.zero;
 
-        // A note specific to owner authority:
-        // Side Note:  Specific to Owner Authoritative
+        // A note specific to owner authority (see the note above why this would not work for Netcode for GameObjects
+        // v1.8.0 & 1.8.1):
         // Setting the position works as and can be set in OnNetworkSpawn server-side unless there is a
         // CharacterController that is enabled by default on the authoritative side. With CharacterController, it
         // needs to be disabled by default (i.e. in Awake), the server applies the position (OnNetworkSpawn), and then
