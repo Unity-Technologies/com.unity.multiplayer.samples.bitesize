ce7f950f3d3dfaf9a09c9649af1fd86fd510d965
Bastien Humeau <bastienh@unity3d.com>
2024-04-18 11:46:30 -0400

    Fix: Adding comments on the rigidbody changes when parenting to be clear in what is being done [MTT-8377]

--- Basic/ClientDriven/Assets/Scripts/ServerIngredientPhysics.cs
+++ Basic/ClientDriven/Assets/Scripts/ServerIngredientPhysics.cs
@@ -1,4 +1,3 @@
-using System;
 using Unity.Netcode;
 using Unity.Netcode.Components;
 using UnityEngine;
@@ -14,13 +13,24 @@ public class ServerIngredientPhysics : NetworkBehaviour
 
     public override void OnNetworkObjectParentChanged(NetworkObject parentNetworkObject)
     {
-        SetPhysics(parentNetworkObject == null);
-    }
-
-    void SetPhysics(bool isEnabled)
-    {
-        m_Rigidbody.isKinematic = !isEnabled;
-        m_Rigidbody.interpolation = isEnabled ? RigidbodyInterpolation.Interpolate : RigidbodyInterpolation.None;
-        m_NetworkTransform.InLocalSpace = !isEnabled;
+        // The physic needs to be disabled when the ingredient is parented so that it's following the parent instead of moving freely.
+        if (parentNetworkObject != null)
+        {
+            // Setting isKinematic to true will prevent the object from being affected by the physic in the game,
+            // which will let it follow the parented player and not being bumped around by other rigidbodies.
+            m_Rigidbody.isKinematic = true;
+            // Rigibodies in Unity ignore parenting when updating their transform position.
+            // Changing the interpolation to None when a Kinematic rigibody is the child of another rigidbody
+            // will allow the object to keep the same local position.
+            m_Rigidbody.interpolation = RigidbodyInterpolation.None;
+            m_NetworkTransform.InLocalSpace = true;
+        }
+        // If the ingredient is not parented anymore, the physic is turned back on.
+        else
+        {
+            m_Rigidbody.isKinematic = false;
+            m_Rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+            m_NetworkTransform.InLocalSpace = false;
+        }
     }
 }
