973a9a7ee9c44298db27dd05339a20d559c0c3e9
Bastien Humeau <bastienh@unity3d.com>
2024-04-24 13:48:50 -0400

    Putting the code back into a method to avoid duplication

--- Basic/ClientDriven/Assets/Scripts/ServerIngredientPhysics.cs
+++ Basic/ClientDriven/Assets/Scripts/ServerIngredientPhysics.cs
@@ -13,24 +13,18 @@ public class ServerIngredientPhysics : NetworkBehaviour
 
     public override void OnNetworkObjectParentChanged(NetworkObject parentNetworkObject)
     {
-        // The physic needs to be disabled when the ingredient is parented so that it's following the parent instead of moving freely.
-        if (parentNetworkObject != null)
-        {
-            // Setting isKinematic to true will prevent the object from being affected by the physic in the game,
-            // which will let it follow the parented player and not being bumped around by other rigidbodies.
-            m_Rigidbody.isKinematic = true;
-            // Rigibodies in Unity ignore parenting when updating their transform position.
-            // Changing the interpolation to None when a Kinematic rigibody is the child of another rigidbody
-            // will allow the object to keep the same local position.
-            m_Rigidbody.interpolation = RigidbodyInterpolation.None;
-            m_NetworkTransform.InLocalSpace = true;
-        }
-        // If the ingredient is not parented anymore, the physic is turned back on.
-        else
-        {
-            m_Rigidbody.isKinematic = false;
-            m_Rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
-            m_NetworkTransform.InLocalSpace = false;
-        }
+        // The ingredient's physic needs to be disabled when the ingredient is parented so that it's following the parent instead of moving freely.
+        SetKinematic(parentNetworkObject != null);
+    }
+
+    private void SetKinematic(bool isEnabled)
+    {
+        // Setting isKinematic to true will prevent the object from being affected by the physic forces in the game.
+        m_Rigidbody.isKinematic = isEnabled;
+        // When parented, rigidbody children are only moving because they receive their parent forces along with their own physic.
+        // When kinematic is true, they will ignore their parent forces and stay at the same world place.
+        // Changing the interpolation to None in that case will allow the object to keep the same local position instead of the world position.
+        m_Rigidbody.interpolation = isEnabled ? RigidbodyInterpolation.None : RigidbodyInterpolation.Interpolate;
+        m_NetworkTransform.InLocalSpace = isEnabled;
     }
 }
