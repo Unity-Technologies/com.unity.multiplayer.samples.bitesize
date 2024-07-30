777072525521394abba06ce271cde763eb6b7c4c
Fernando Cortez <fernando.cortez@unity3d.com>
2024-07-30 17:07:55 -0400

    cleanup, avatar set with AvatarTransform

--- Experimental/DistributedAuthoritySample/Assets/Scripts/Input/AvatarInputs.cs
+++ Experimental/DistributedAuthoritySample/Assets/Scripts/Input/AvatarInputs.cs
@@ -1,73 +1,76 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class AvatarInputs : MonoBehaviour
+namespace com.unity.multiplayer.samples.distributed_authority.input
 {
-    [Header("Character Input Values")]
-    public Vector2 move;
-    public Vector2 look;
-    public bool jump;
-    public bool sprint;
+    public class AvatarInputs : MonoBehaviour
+    {
+        [Header("Character Input Values")]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
 
-    [Header("Movement Settings")]
-    public bool analogMovement;
+        [Header("Movement Settings")]
+        public bool analogMovement;
 
-    [Header("Mouse Cursor Settings")]
-    public bool cursorLocked = true;
-    public bool cursorInputForLook = true;
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+        public bool cursorInputForLook = true;
 
 #if ENABLE_INPUT_SYSTEM
-    public void OnMove(InputValue value)
-    {
-        MoveInput(value.Get<Vector2>());
-    }
-
-    public void OnLook(InputValue value)
-    {
-        if(cursorInputForLook)
+        public void OnMove(InputValue value)
         {
-            LookInput(value.Get<Vector2>());
+            MoveInput(value.Get<Vector2>());
         }
-    }
 
-    public void OnJump(InputValue value)
-    {
-        JumpInput(value.isPressed);
-    }
+        public void OnLook(InputValue value)
+        {
+            if (cursorInputForLook)
+            {
+                LookInput(value.Get<Vector2>());
+            }
+        }
 
-    public void OnSprint(InputValue value)
-    {
-        SprintInput(value.isPressed);
-    }
+        public void OnJump(InputValue value)
+        {
+            JumpInput(value.isPressed);
+        }
+
+        public void OnSprint(InputValue value)
+        {
+            SprintInput(value.isPressed);
+        }
 #endif
 
-    public void MoveInput(Vector2 newMoveDirection)
-    {
-        move = newMoveDirection;
-    }
+        public void MoveInput(Vector2 newMoveDirection)
+        {
+            move = newMoveDirection;
+        }
 
-    public void LookInput(Vector2 newLookDirection)
-    {
-        look = newLookDirection;
-    }
+        public void LookInput(Vector2 newLookDirection)
+        {
+            look = newLookDirection;
+        }
 
-    public void JumpInput(bool newJumpState)
-    {
-        jump = newJumpState;
-    }
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
 
-    public void SprintInput(bool newSprintState)
-    {
-        sprint = newSprintState;
-    }
+        public void SprintInput(bool newSprintState)
+        {
+            sprint = newSprintState;
+        }
 
-    private void OnApplicationFocus(bool hasFocus)
-    {
-        SetCursorState(cursorLocked);
-    }
+        void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(cursorLocked);
+        }
 
-    private void SetCursorState(bool newState)
-    {
-        Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        }
     }
 }
--- Experimental/DistributedAuthoritySample/Assets/Scripts/Player/AvatarTransform.cs
+++ Experimental/DistributedAuthoritySample/Assets/Scripts/Player/AvatarTransform.cs
@@ -1,234 +1,270 @@
+using Unity.Netcode.Components;
+using Unity.Netcode.Editor;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using com.unity.multiplayer.samples.distributed_authority.input;
 #if UNITY_EDITOR
 using UnityEditor;
-
-/// <summary>
-/// The custom editor for the <see cref="AvatarTransform"/> component.
-/// </summary>
-[CustomEditor(typeof(AvatarTransform), true)]
-public class AvatarTransformEditor : BaseObjectMotionHandlerEditor
-{
-    SerializedProperty m_PlayerInput;
-    SerializedProperty m_AvatarInputs;
-    SerializedProperty m_WalkSpeed;
-    SerializedProperty m_SprintSpeed;
-    SerializedProperty m_Acceleration;
-    SerializedProperty m_DragCoefficient;
-    SerializedProperty m_AirControlFactor;
-    SerializedProperty m_JumpImpulse;
-    SerializedProperty m_CustomGravityMultiplier;
-    SerializedProperty m_RotationSpeed;
-    SerializedProperty m_GroundCheckDistance;
-
-    public override void OnEnable()
-    {
-        m_PlayerInput = serializedObject.FindProperty(nameof(AvatarTransform.PlayerInput));
-        m_AvatarInputs = serializedObject.FindProperty(nameof(AvatarTransform.AvatarInputs));
-        m_WalkSpeed = serializedObject.FindProperty(nameof(AvatarTransform.WalkSpeed));
-        m_SprintSpeed = serializedObject.FindProperty(nameof(AvatarTransform.SprintSpeed));
-        m_Acceleration = serializedObject.FindProperty(nameof(AvatarTransform.Acceleration));
-        m_DragCoefficient = serializedObject.FindProperty(nameof(AvatarTransform.DragCoefficient));
-        m_AirControlFactor = serializedObject.FindProperty(nameof(AvatarTransform.AirControlFactor));
-        m_JumpImpulse = serializedObject.FindProperty(nameof(AvatarTransform.JumpImpusle));
-        m_CustomGravityMultiplier = serializedObject.FindProperty(nameof(AvatarTransform.CustomGravityMultiplier));
-        m_RotationSpeed = serializedObject.FindProperty(nameof(AvatarTransform.RotationSpeed));
-        m_GroundCheckDistance = serializedObject.FindProperty(nameof(AvatarTransform.GroundCheckDistance));
-
-        base.OnEnable();
-    }
-
-    public override void OnInspectorGUI()
-    {
-        var avatarTransform = target as AvatarTransform;
-        avatarTransform.PropertiesVisible = EditorGUILayout.BeginFoldoutHeaderGroup(avatarTransform.PropertiesVisible, $"{nameof(AvatarTransform)} Properties");
-        if (avatarTransform.PropertiesVisible)
-        {
-            EditorGUILayout.EndFoldoutHeaderGroup();
-            EditorGUILayout.PropertyField(m_PlayerInput);
-            EditorGUILayout.PropertyField(m_AvatarInputs);
-            EditorGUILayout.PropertyField(m_WalkSpeed);
-            EditorGUILayout.PropertyField(m_SprintSpeed);
-            EditorGUILayout.PropertyField(m_Acceleration);
-            EditorGUILayout.PropertyField(m_DragCoefficient);
-            EditorGUILayout.PropertyField(m_AirControlFactor);
-            EditorGUILayout.PropertyField(m_JumpImpulse);
-            EditorGUILayout.PropertyField(m_CustomGravityMultiplier);
-            EditorGUILayout.PropertyField(m_RotationSpeed);
-            EditorGUILayout.PropertyField(m_GroundCheckDistance);
-        }
-        else
-        {
-            EditorGUILayout.EndFoldoutHeaderGroup();
-        }
-        EditorGUILayout.Space();
-        base.OnInspectorGUI();
-    }
-}
 #endif
 
-[RequireComponent(typeof(Rigidbody))]
-public class AvatarTransform : BaseObjectMotionHandler
+namespace com.unity.multiplayer.samples.distributed_authority.gameplay
 {
 #if UNITY_EDITOR
-    public bool PropertiesVisible = false;
+    /// <summary>
+    /// The custom editor for the <see cref="AvatarTransform"/> component.
+    /// </summary>
+    [CustomEditor(typeof(AvatarTransform), true)]
+    public class AvatarTransformEditor : NetworkTransformEditor
+    {
+        SerializedProperty m_Rigidbody;
+        SerializedProperty m_PlayerInput;
+        SerializedProperty m_AvatarInputs;
+        SerializedProperty m_WalkSpeed;
+        SerializedProperty m_SprintSpeed;
+        SerializedProperty m_Acceleration;
+        SerializedProperty m_DragCoefficient;
+        SerializedProperty m_AirControlFactor;
+        SerializedProperty m_JumpImpulse;
+        SerializedProperty m_CustomGravityMultiplier;
+        SerializedProperty m_RotationSpeed;
+        SerializedProperty m_GroundCheckDistance;
+
+        public override void OnEnable()
+        {
+            m_Rigidbody = serializedObject.FindProperty(nameof(AvatarTransform.m_Rigidbody));
+            m_PlayerInput = serializedObject.FindProperty(nameof(AvatarTransform.m_PlayerInput));
+            m_AvatarInputs = serializedObject.FindProperty(nameof(AvatarTransform.m_AvatarInputs));
+            m_WalkSpeed = serializedObject.FindProperty(nameof(AvatarTransform.m_WalkSpeed));
+            m_SprintSpeed = serializedObject.FindProperty(nameof(AvatarTransform.m_SprintSpeed));
+            m_Acceleration = serializedObject.FindProperty(nameof(AvatarTransform.m_Acceleration));
+            m_DragCoefficient = serializedObject.FindProperty(nameof(AvatarTransform.m_DragCoefficient));
+            m_AirControlFactor = serializedObject.FindProperty(nameof(AvatarTransform.m_AirControlFactor));
+            m_JumpImpulse = serializedObject.FindProperty(nameof(AvatarTransform.m_JumpImpusle));
+            m_CustomGravityMultiplier = serializedObject.FindProperty(nameof(AvatarTransform.m_CustomGravityMultiplier));
+            m_RotationSpeed = serializedObject.FindProperty(nameof(AvatarTransform.m_RotationSpeed));
+            m_GroundCheckDistance = serializedObject.FindProperty(nameof(AvatarTransform.m_GroundCheckDistance));
+
+            base.OnEnable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            var avatarTransform = target as AvatarTransform;
+            avatarTransform.PropertiesVisible = EditorGUILayout.BeginFoldoutHeaderGroup(avatarTransform.PropertiesVisible, $"{nameof(AvatarTransform)} Properties");
+            if (avatarTransform.PropertiesVisible)
+            {
+                EditorGUILayout.EndFoldoutHeaderGroup();
+                EditorGUILayout.PropertyField(m_Rigidbody);
+                EditorGUILayout.PropertyField(m_PlayerInput);
+                EditorGUILayout.PropertyField(m_AvatarInputs);
+                EditorGUILayout.PropertyField(m_WalkSpeed);
+                EditorGUILayout.PropertyField(m_SprintSpeed);
+                EditorGUILayout.PropertyField(m_Acceleration);
+                EditorGUILayout.PropertyField(m_DragCoefficient);
+                EditorGUILayout.PropertyField(m_AirControlFactor);
+                EditorGUILayout.PropertyField(m_JumpImpulse);
+                EditorGUILayout.PropertyField(m_CustomGravityMultiplier);
+                EditorGUILayout.PropertyField(m_RotationSpeed);
+                EditorGUILayout.PropertyField(m_GroundCheckDistance);
+            }
+            else
+            {
+                EditorGUILayout.EndFoldoutHeaderGroup();
+            }
+
+            EditorGUILayout.Space();
+            base.OnInspectorGUI();
+        }
+    }
 #endif
 
-    public PlayerInput PlayerInput;
-    public AvatarInputs AvatarInputs;
-    public float WalkSpeed;
-    public float SprintSpeed;
-    public float Acceleration;
-    public float DragCoefficient;
-    public float AirControlFactor;
-    public float JumpImpusle;
-    public float CustomGravityMultiplier;
-    public float RotationSpeed;
-    public float GroundCheckDistance;
-
-    Vector3 m_Movement;
-
-    // grab jump state from input and clear after consumed
-    bool m_Jump;
-
-    // cached grounded check
-    bool m_IsGrounded;
-
-    RaycastHit[] m_RaycastHits = new RaycastHit[1];
-    Ray m_Ray;
-
-    public override void OnNetworkSpawn()
+    [RequireComponent(typeof(Rigidbody))]
+    public class AvatarTransform : NetworkTransform
     {
-        base.OnNetworkSpawn();
+#if UNITY_EDITOR
+        public bool PropertiesVisible = false;
+#endif
 
-        if (!HasAuthority)
+        [SerializeField]
+        internal Rigidbody m_Rigidbody;
+
+        [SerializeField]
+        internal PlayerInput m_PlayerInput;
+
+        [SerializeField]
+        internal AvatarInputs m_AvatarInputs;
+
+        [SerializeField]
+        internal float m_WalkSpeed;
+
+        [SerializeField]
+        internal float m_SprintSpeed;
+
+        [SerializeField]
+        internal float m_Acceleration;
+
+        [SerializeField]
+        internal float m_DragCoefficient;
+
+        [SerializeField]
+        internal float m_AirControlFactor;
+
+        [SerializeField]
+        internal float m_JumpImpusle;
+
+        [SerializeField]
+        internal float m_CustomGravityMultiplier;
+
+        [SerializeField]
+        internal float m_RotationSpeed;
+
+        [SerializeField]
+        internal float m_GroundCheckDistance;
+
+        Vector3 m_Movement;
+
+        // grab jump state from input and clear after consumed
+        bool m_Jump;
+
+        // cached grounded check
+        bool m_IsGrounded;
+
+        RaycastHit[] m_RaycastHits = new RaycastHit[1];
+
+        Ray m_Ray;
+
+        public override void OnNetworkSpawn()
         {
-            return;
+            base.OnNetworkSpawn();
+
+            if (!HasAuthority)
+            {
+                return;
+            }
+
+            m_PlayerInput.enabled = true;
+
+            m_Rigidbody.isKinematic = false;
+
+            // Freeze rotation on the x and z axes to prevent toppling
+            m_Rigidbody.freezeRotation = true;
+
+            var spawnPosition = new Vector3(0f, 1.5f, 0f);
+            transform.SetPositionAndRotation(position: spawnPosition, rotation: Quaternion.identity);
+            m_Rigidbody.position = spawnPosition;
+            m_Rigidbody.linearVelocity = Vector3.zero;
         }
 
-        PlayerInput.enabled = true;
-
-        Rigidbody.isKinematic = false;
-
-        // Freeze rotation on the x and z axes to prevent toppling
-        Rigidbody.freezeRotation = true;
-
-        var spawnPosition = new Vector3(0f, 1.5f, 0f);
-        transform.SetPositionAndRotation(position: spawnPosition, rotation: Quaternion.identity);
-        Rigidbody.position = spawnPosition;
-        SetObjectVelocity(Vector3.zero);
-    }
-
-    void Update()
-    {
-        if (!HasAuthority)
+        void Update()
         {
-            return;
+            if (!IsSpawned || !HasAuthority)
+            {
+                return;
+            }
+
+            m_Movement = new Vector3(m_AvatarInputs.move.x, 0, m_AvatarInputs.move.y).normalized;
+
+            // Handle rotation based on input direction
+            if (m_Movement.magnitude >= 0.1f)
+            {
+                var targetAngle = Mathf.Atan2(m_Movement.x, m_Movement.z) * Mathf.Rad2Deg;
+                var targetRotation = Quaternion.Euler(0, targetAngle, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * m_RotationSpeed);
+            }
+
+            if (IsGrounded() && m_AvatarInputs.jump)
+            {
+                m_Jump = true;
+                m_AvatarInputs.jump = false;
+            }
         }
 
-        m_Movement = new Vector3(AvatarInputs.move.x, 0, AvatarInputs.move.y).normalized;
-
-        // Handle rotation based on input direction
-        if (m_Movement.magnitude >= 0.1f)
+        void ApplyMovement()
         {
-            var targetAngle = Mathf.Atan2(m_Movement.x, m_Movement.z) * Mathf.Rad2Deg;
-            var targetRotation = Quaternion.Euler(0, targetAngle, 0);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * RotationSpeed);
+            if (Mathf.Approximately(m_Movement.magnitude, 0f))
+            {
+                return;
+            }
+
+            var velocity = m_Rigidbody.linearVelocity;
+            var desiredVelocity = m_Movement * (m_AvatarInputs.sprint ? m_SprintSpeed : m_WalkSpeed);
+            var targetVelocity = new Vector3(desiredVelocity.x, velocity.y, desiredVelocity.z);
+            var velocityChange = targetVelocity - velocity;
+
+            if (m_IsGrounded)
+            {
+                // Apply force proportional to acceleration while grounded
+                var force = velocityChange * m_Acceleration;
+                m_Rigidbody.AddForce(force, ForceMode.Acceleration);
+            }
+            else
+            {
+                // Apply reduced force in the air for air control
+                var force = velocityChange * (m_Acceleration * m_AirControlFactor);
+                m_Rigidbody.AddForce(force, ForceMode.Acceleration);
+            }
         }
 
-        if (AvatarInputs.jump && IsGrounded())
+        void ApplyJump()
         {
-            m_Jump = true;
-            AvatarInputs.jump = false;
-        }
-    }
-
-    void ApplyMovement()
-    {
-        if (Mathf.Approximately(m_Movement.magnitude, 0f))
-        {
-            return;
+            if (m_IsGrounded && m_Jump)
+            {
+                m_Rigidbody.AddForce(Vector3.up * m_JumpImpusle, ForceMode.Impulse);
+                m_Jump = false;
+            }
         }
 
-        var velocity = GetObjectVelocity();
-        var desiredVelocity = m_Movement * (AvatarInputs.sprint ? SprintSpeed : WalkSpeed);
-        var targetVelocity = new Vector3(desiredVelocity.x, velocity.y, desiredVelocity.z);
-        var velocityChange = targetVelocity - velocity;
-
-        if (m_IsGrounded)
+        void UpdateGroundedStatus()
         {
-            // Apply force proportional to acceleration while grounded
-            var force = velocityChange * Acceleration;
-            Rigidbody.AddForce(force, ForceMode.Acceleration);
-        }
-        else
-        {
-            // Apply reduced force in the air for air control
-            var force = velocityChange * (Acceleration * AirControlFactor);
-            Rigidbody.AddForce(force, ForceMode.Acceleration);
-        }
-    }
-
-    void ApplyJump()
-    {
-        if (m_IsGrounded && m_Jump)
-        {
-            Rigidbody.AddForce(Vector3.up * JumpImpusle, ForceMode.Impulse);
-            m_Jump = false;
-        }
-    }
-
-    void UpdateGroundedStatus()
-    {
-        m_IsGrounded = IsGrounded();
-    }
-
-    bool IsGrounded()
-    {
-        // Perform a raycast to check if the character is grounded
-        m_Ray.origin = transform.position;
-        m_Ray.direction = Vector3.down;
-        return Physics.RaycastNonAlloc(m_Ray, m_RaycastHits, GroundCheckDistance) > 0;
-    }
-
-    void FixedUpdate()
-    {
-        if (!IsSpawned || !HasAuthority || Rigidbody != null && Rigidbody.isKinematic)
-        {
-            return;
+            m_IsGrounded = IsGrounded();
         }
 
-        UpdateGroundedStatus();
-
-        ApplyMovement();
-
-        ApplyJump();
-
-        ApplyDrag();
-
-        ApplyCustomGravity();
-    }
-
-    void ApplyDrag()
-    {
-        var groundVelocity = GetObjectVelocity();
-        groundVelocity.y = 0f;
-        if (groundVelocity.magnitude > 0f)
+        bool IsGrounded()
         {
-            // Apply deceleration force to stop movement
-            var dragForce = -DragCoefficient * groundVelocity.magnitude * groundVelocity;
-            Rigidbody.AddForce(dragForce, ForceMode.Acceleration);
+            // Perform a raycast to check if the character is grounded
+            m_Ray.origin = m_Rigidbody.worldCenterOfMass;
+            m_Ray.direction = Vector3.down;
+            return Physics.RaycastNonAlloc(m_Ray, m_RaycastHits, m_GroundCheckDistance) > 0;
         }
-    }
 
-    void ApplyCustomGravity()
-    {
-        // custom gravity
-        if (!m_IsGrounded)
+        void FixedUpdate()
         {
-            var customGravity = Physics.gravity * (CustomGravityMultiplier - 1);
-            Rigidbody.AddForce(customGravity, ForceMode.Acceleration);
+            if (!IsSpawned || !HasAuthority || m_Rigidbody != null && m_Rigidbody.isKinematic)
+            {
+                return;
+            }
+
+            UpdateGroundedStatus();
+
+            ApplyMovement();
+
+            ApplyJump();
+
+            ApplyDrag();
+
+            ApplyCustomGravity();
+        }
+
+        void ApplyDrag()
+        {
+            var groundVelocity = m_Rigidbody.linearVelocity;
+            groundVelocity.y = 0f;
+            if (groundVelocity.magnitude > 0f)
+            {
+                // Apply deceleration force to stop movement
+                var dragForce = -m_DragCoefficient * groundVelocity.magnitude * groundVelocity;
+                m_Rigidbody.AddForce(dragForce, ForceMode.Acceleration);
+            }
+        }
+
+        void ApplyCustomGravity()
+        {
+            // custom gravity
+            if (!m_IsGrounded)
+            {
+                var customGravity = Physics.gravity * (m_CustomGravityMultiplier - 1);
+                m_Rigidbody.AddForce(customGravity, ForceMode.Acceleration);
+            }
         }
     }
 }
