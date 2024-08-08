a4b1009a2d31bd950c34d49db205217fe76bb33a
Fernando Cortez <fernando.cortez@unity3d.com>
2024-08-08 11:44:14 -0400

    feat: Distributed Authority Sample player locomotion [MTT-8898] (#200)
    * initial project commit
    
    * Added: proposed folder structure from TDD
    
    * consolidating settings folders
    
    * adding ignored temp script for testing connection
    
    * importing asteroids base classes & locomotion controller wip
    
    * reworked drag, inheriting from baseobject movement
    
    * cleanup, avatar set with AvatarTransform
    
    * distinguishing spawmed players with name in hierarchy
    
    * moving AvatarTransformEditor to own Editor subdirectory class, cleanup
    
    * formatting changes
    
    ---------
    
    Co-authored-by: Elfi Kühndorf <elfi.kuhndorf@unity3d.com>

---
+++ Experimental/DistributedAuthoritySample/Assets/Scripts/Input/AvatarInputs.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace com.unity.multiplayer.samples.distributed_authority.input
+{
+    class AvatarInputs : MonoBehaviour
+    {
+        [Header("Character Input Values")]
+        [SerializeField]
+        internal Vector2 Move;
+        [SerializeField]
+        internal Vector2 Look;
+        [SerializeField]
+        internal bool Jump;
+        [SerializeField]
+        internal bool Sprint;
+
+        [Header("Movement Settings")]
+        [SerializeField]
+        internal bool AnalogMovement;
+
+        [Header("Mouse Cursor Settings")]
+        [SerializeField]
+        internal bool CursorLocked = true;
+        [SerializeField]
+        internal bool CursorInputForLook = true;
+
+#if ENABLE_INPUT_SYSTEM
+        void OnMove(InputValue value)
+        {
+            MoveInput(value.Get<Vector2>());
+        }
+
+        void OnLook(InputValue value)
+        {
+            if (CursorInputForLook)
+            {
+                LookInput(value.Get<Vector2>());
+            }
+        }
+
+        void OnJump(InputValue value)
+        {
+            JumpInput(value.isPressed);
+        }
+
+        void OnSprint(InputValue value)
+        {
+            SprintInput(value.isPressed);
+        }
+#endif
+
+        void MoveInput(Vector2 newMoveDirection)
+        {
+            Move = newMoveDirection;
+        }
+
+        void LookInput(Vector2 newLookDirection)
+        {
+            Look = newLookDirection;
+        }
+
+        void JumpInput(bool newJumpState)
+        {
+            Jump = newJumpState;
+        }
+
+        void SprintInput(bool newSprintState)
+        {
+            Sprint = newSprintState;
+        }
+
+        void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(CursorLocked);
+        }
+
+        void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+    }
+}
