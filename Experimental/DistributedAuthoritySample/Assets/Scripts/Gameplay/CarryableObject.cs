429dd2ca19b00ba989e6584ad7ef57106dc503f2
Elfi Kühndorf <elfi.kuhndorf@unity3d.com>
2024-08-29 18:53:02 +0200

    change activation and deactivation functionality from rubble and chest
    change activation from renderer and collider instead of deactivating whole gameObject

--- Experimental/DistributedAuthoritySample/Assets/Scripts/Gameplay/CarryableObject.cs
+++ Experimental/DistributedAuthoritySample/Assets/Scripts/Gameplay/CarryableObject.cs
@@ -4,134 +4,130 @@ using Unity.Netcode;
 
 namespace Unity.Multiplayer.Samples.SocialHub.Gameplay
 {
-    public class CarryableObject : NetworkBehaviour
+public class CarryableObject : NetworkBehaviour
+{
+    public GameObject LeftHand;
+    public GameObject RightHand;
+    public int Health = 1;
+    public GameObject destructionVFX;
+
+    private int previousHealth;
+
+    public int CurrentHealth
     {
-        public GameObject LeftHand;
-        public GameObject RightHand;
-        public int Health = 1;
-        public GameObject destructionVFX;
-
-        private int previousHealth;
-
-        // Add health component to object
-        public int CurrentHealth
+        get => Health;
+        set
         {
-            get => Health;
-            set
+            Health = value;
+            if (Health <= 0)
             {
-                Health = value;
-                if (Health <= 0)
-                {
-                    DestroyObject();
-                }
+                DestroyObject();
             }
-        }
-
-        private void Start()
-        {
-            previousHealth = Health;
-            if (IsServer && IsOwner)
-            {
-                NetworkObject.Spawn();
-            }
-        }
-
-        private void Update()
-        {
-            // Check if Health value has changed in the Inspector
-            if (previousHealth != Health)
-            {
-                CurrentHealth = Health;
-                previousHealth = Health;
-            }
-        }
-
-        protected virtual void DestroyObject()
-        {
-            Debug.Log("Object Destroyed");
-            StartCoroutine(DeferredDespawn());
-        }
-
-        protected IEnumerator DeferredDespawn()
-        {
-            Debug.Log("DeferredDespawn started");
-
-            // Disable renderers or the entire game object to visually hide the chest
-            ChangeChestVisuals(false);
-
-            var vfxInstance = Instantiate(destructionVFX, transform.position, Quaternion.identity);
-            var particleSystem = vfxInstance.GetComponent<ParticleSystem>();
-
-            if (particleSystem != null)
-            {
-                particleSystem.Play();
-                float totalWaitTime = particleSystem.main.duration;
-
-                yield return new WaitForSeconds(totalWaitTime);
-            }
-
-            // Inform other clients to play VFX and spawn rubble locally
-            NotifyClientsOfDestruction(transform.position);
-
-            Debug.Log("VFX Destroyed");
-            Destroy(vfxInstance);
-
-            // set timer to despawn rubble and reenable chest
-            yield return new WaitForSeconds(5f);
-            ChangeChestVisuals(true);
-        }
-
-        private void ChangeChestVisuals(bool enable)
-        {
-            // Disable all renderers to hide the chest visually
-            Renderer[] renderers = GetComponentsInChildren<Renderer>();
-            foreach (Renderer renderer in renderers)
-            {
-                renderer.enabled = enable;
-            }
-
-            // Disable all colliders to make the chest non-interactive
-            Collider[] colliders = GetComponentsInChildren<Collider>();
-            foreach (Collider collider in colliders)
-            {
-                collider.enabled = enable;
-            }
-        }
-
-        private void NotifyClientsOfDestruction(Vector3 position)
-        {
-            if (IsOwner)
-            {
-                InformOtherClientsOfDestructionClientRpc(position);
-            }
-        }
-
-        [ClientRpc]
-        private void InformOtherClientsOfDestructionClientRpc(Vector3 position)
-        {
-            if (!IsOwner)
-            {
-                PlayDestructionVFX(position);
-                SpawnRubble(position);
-            }
-        }
-
-        private void PlayDestructionVFX(Vector3 position)
-        {
-            GameObject vfxInstance = Instantiate(destructionVFX, position, Quaternion.identity);
-            var particleSystem = vfxInstance.GetComponent<ParticleSystem>();
-
-            if (particleSystem != null)
-            {
-                particleSystem.Play();
-                float totalWaitTime = particleSystem.main.duration;
-                Destroy(vfxInstance, totalWaitTime);
-            }
-        }
-
-        protected virtual void SpawnRubble(Vector3 position)
-        {
-           // Instantiate(rubblePrefab, position, Quaternion.identity);
         }
     }
+
+    private void Start()
+    {
+        previousHealth = Health;
+        if (IsServer && IsOwner)
+        {
+            NetworkObject.Spawn();
+        }
+    }
+
+    private void Update()
+    {
+        if (previousHealth != Health)
+        {
+            CurrentHealth = Health;
+            previousHealth = Health;
+        }
+    }
+
+    protected virtual void DestroyObject()
+    {
+        Debug.Log("Object Destroyed");
+        StartCoroutine(DeferredDespawn());
+    }
+
+    protected IEnumerator DeferredDespawn()
+    {
+        Debug.Log("DeferredDespawn started");
+
+        ChangeChestVisuals(false);
+        var vfxInstance = Instantiate(destructionVFX, transform.position, Quaternion.identity);
+        var particleSystem = vfxInstance.GetComponent<ParticleSystem>();
+
+        if (particleSystem != null)
+        {
+            particleSystem.Play();
+            float totalWaitTime = particleSystem.main.duration;
+
+            yield return new WaitForSeconds(totalWaitTime);
+        }
+
+        NotifyClientsOfDestruction(transform.position);
+        Debug.Log("VFX Destroyed");
+        Destroy(vfxInstance);
+
+        yield return new WaitForSeconds(5f);
+        ChangeChestVisuals(true);
+    }
+
+    protected virtual void ChangeChestVisuals(bool enable)
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.enabled = enable;
+        }
+
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = enable;
+        }
+
+        if (this is Chest chest)
+        {
+            chest.ChangeRubbleVisuals(!enable, Vector3.zero); // Ensure the rubble stays active when chest is inactive and vice-versa
+        }
+    }
+
+    private void NotifyClientsOfDestruction(Vector3 position)
+    {
+        if (IsOwner)
+        {
+            InformOtherClientsOfDestructionClientRpc(position);
+        }
+    }
+
+    [ClientRpc]
+    private void InformOtherClientsOfDestructionClientRpc(Vector3 position)
+    {
+        if (!IsOwner)
+        {
+            PlayDestructionVFX(position);
+            SpawnRubble(position);
+        }
+    }
+
+    private void PlayDestructionVFX(Vector3 position)
+    {
+        GameObject vfxInstance = Instantiate(destructionVFX, position, Quaternion.identity);
+        var particleSystem = vfxInstance.GetComponent<ParticleSystem>();
+
+        if (particleSystem != null)
+        {
+            particleSystem.Play();
+            float totalWaitTime = particleSystem.main.duration;
+            Destroy(vfxInstance, totalWaitTime);
+        }
+    }
+
+    protected virtual void SpawnRubble(Vector3 position)
+    {
+        // Intended to be overridden by derived classes.
+    }
+}
 }
--- Experimental/DistributedAuthoritySample/Assets/Scripts/Gameplay/Chest.cs
+++ Experimental/DistributedAuthoritySample/Assets/Scripts/Gameplay/Chest.cs
@@ -6,45 +6,57 @@ namespace Unity.Multiplayer.Samples.SocialHub.Gameplay
 {
     public class Chest : CarryableObject
     {
-        protected override void DestroyObject()
+        internal GameObject rubble;
+
+        private void Awake()
         {
-            Vector3 spawnPosition = transform.position; // Store the position before base is called
-            base.DestroyObject(); // Call the base method to propagate the event and play VFX
+            rubble = transform.Find("Rubble_Chest").gameObject; // Make sure the child object's name is correct
+            if (rubble != null)
+            {
+                Vector3 spawnPosition = transform.position;
+                ChangeRubbleVisuals(false, spawnPosition);
+            }
+        }
+
+        /*protected override void DestroyObject()
+        {
+            Vector3 spawnPosition = transform.position;
+            base.DestroyObject();
             SpawnRubble(spawnPosition);
-        }
+        }*/
 
-        protected override void SpawnRubble(Vector3 position)
+        /*protected override void SpawnRubble(Vector3 position)
         {
-            //set the childobject rubble to active
+            ChangeRubbleVisuals(true, position);
+        }*/
 
-            GameObject rubblePrefab = gameObject.GetComponentInChildren<NetworkObject>().gameObject;
-            rubblePrefab.SetActive(true);
-            /*// set the rubble after 5 seconds to inactive
-            var waitForSecondsEnumerable = RubbleDeactivate(rubblePrefab);*/
+        protected internal void ChangeRubbleVisuals(bool enable, Vector3 transform)
+        {
+            if (rubble != null)
+            {
+                // Enable or disable renderers
+                Renderer[] renderers = rubble.GetComponentsInChildren<Renderer>();
+                foreach (Renderer renderer in renderers)
+                {
+                    renderer.enabled = enable;
+                }
+
+                // Enable or disable colliders
+                Collider[] colliders = rubble.GetComponentsInChildren<Collider>();
+                foreach (Collider collider in colliders)
+                {
+                    collider.enabled = enable;
+                    rubble.gameObject.transform.position = transform;
+                }
+            }
         }
-
 
         private IEnumerable<WaitForSeconds> RubbleDeactivate(GameObject rubblePrefab)
         {
             yield return new WaitForSeconds(5f);
-            rubblePrefab.SetActive(false);
-        }
-
-        private void ChangeRubbleVisuals(bool enable)
-        {
-            // Disable all renderers to hide the chest visually
-            Renderer[] renderers = GetComponentsInChildren<Renderer>();
-            foreach (Renderer renderer in renderers)
-            {
-                renderer.enabled = enable;
-            }
-
-            // Disable all colliders to make the chest non-interactive
-            Collider[] colliders = GetComponentsInChildren<Collider>();
-            foreach (Collider collider in colliders)
-            {
-                collider.enabled = enable;
-            }
+            ChangeRubbleVisuals(false, transform.position);
         }
     }
+
 }
+
