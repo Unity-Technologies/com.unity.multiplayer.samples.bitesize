0a975c981ad55446164fcba1a494ce7ba7575c91
Elfi Kühndorf <elfi.kuhndorf@unity3d.com>
2024-09-02 16:08:24 +0200

    merging functionality of respawn into one script, enabling logic for pottery prefab as well

--- Experimental/DistributedAuthoritySample/Assets/Scripts/Gameplay/CarryableObject.cs
+++ Experimental/DistributedAuthoritySample/Assets/Scripts/Gameplay/CarryableObject.cs
@@ -4,57 +4,87 @@ using Unity.Netcode;
 
 namespace Unity.Multiplayer.Samples.SocialHub.Gameplay
 {
-    public class CarryableObject : NetworkBehaviour
+public class CarryableObject : NetworkBehaviour
+{
+    [Header("General Settings")]
+    public GameObject LeftHand;
+    public GameObject RightHand;
+    public int Health = 1;
+
+    [Header("Destruction Settings")]
+    public GameObject destructionVFX;
+    public GameObject rubblePrefab;
+
+    private int previousHealth;
+    private GameObject spawnedRubble;
+
+    public int CurrentHealth
     {
-        public GameObject LeftHand;
-        public GameObject RightHand;
-        public int Health = 1;
-        public GameObject destructionVFX;
-
-        private int previousHealth;
-
-        public int CurrentHealth
+        get => Health;
+        set
         {
-            get => Health;
-            set
+            Health = value;
+            if (Health <= 0)
             {
-                Health = value;
-                if (Health <= 0)
-                {
-                    DestroyObject();
-                }
+                DestroyObject();
             }
         }
+    }
 
-        private void Start()
+    private void Start()
+    {
+        previousHealth = Health;
+        if (IsOwner)
         {
+            NetworkObject.Spawn();
+        }
+    }
+
+    private void Update()
+    {
+        if (previousHealth != Health)
+        {
+            CurrentHealth = Health;
             previousHealth = Health;
-            if (IsServer && IsOwner)
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        // Ensure rubble initialization on network spawn
+        if (IsServer || IsOwner)
+        {
+            InitializeRubble();
+        }
+    }
+
+    private void InitializeRubble()
+    {
+        if (rubblePrefab != null)
+        {
+            spawnedRubble = Instantiate(rubblePrefab, transform.position, Quaternion.identity);
+            if (spawnedRubble.TryGetComponent<NetworkObject>(out var networkObject))
             {
-                NetworkObject.Spawn();
+                networkObject.Spawn(true);
             }
+            ChangeRubbleVisuals(false); // Initially hide the rubble
         }
+    }
 
-        private void Update()
+    protected virtual void DestroyObject()
+    {
+        Debug.Log("Object Destroyed");
+        StartCoroutine(DeferredDespawn());
+    }
+
+    protected IEnumerator DeferredDespawn()
+    {
+        Debug.Log("DeferredDespawn started");
+
+        ChangeObjectVisuals(false);
+
+        if (destructionVFX != null)
         {
-            if (previousHealth != Health)
-            {
-                CurrentHealth = Health;
-                previousHealth = Health;
-            }
-        }
-
-        protected virtual void DestroyObject()
-        {
-            Debug.Log("Object Destroyed");
-            StartCoroutine(DeferredDespawn());
-        }
-
-        protected IEnumerator DeferredDespawn()
-        {
-            Debug.Log("DeferredDespawn started");
-
-            ChangeChestVisuals(false);
             var vfxInstance = Instantiate(destructionVFX, transform.position, Quaternion.identity);
             var particleSystem = vfxInstance.GetComponent<ParticleSystem>();
 
@@ -64,60 +94,99 @@ namespace Unity.Multiplayer.Samples.SocialHub.Gameplay
                 float totalWaitTime = particleSystem.main.duration;
 
                 yield return new WaitForSeconds(totalWaitTime);
+
+                Destroy(vfxInstance);
             }
-
-            NotifyClientsOfDestruction(transform.position);
-            Debug.Log("VFX Destroyed");
-            Destroy(vfxInstance);
-
-            yield return new WaitForSeconds(5f);
-            ChangeChestVisuals(true);
         }
 
-        protected virtual void ChangeChestVisuals(bool enable)
+        NotifyClientsOfDestruction();
+
+        yield return new WaitForSeconds(5f);
+
+        ChangeObjectVisuals(true);
+    }
+
+    private void ChangeObjectVisuals(bool enable)
+    {
+        // Ensure the object is at ground level when re-enabled
+        var objectPosition = transform.position;
+        objectPosition.y = 0;
+        transform.position = objectPosition;
+        transform.rotation = Quaternion.identity; // Ensure the object is upright when re-enabled
+
+        // Disable or enable renderers
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
         {
-            var carryableGameObject = gameObject;
-            var vector3 = carryableGameObject.transform.position;
-            vector3.y = 0;
-            carryableGameObject.transform.position = vector3; // Ensure the object is at ground level when re-enabled
-            carryableGameObject.transform.rotation = Quaternion.identity; // Ensure the object is upright when re-enabled
-            Renderer[] renderers = GetComponentsInChildren<Renderer>();
+            renderer.enabled = enable;
+        }
+
+        // Disable or enable colliders
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = enable;
+        }
+
+        ChangeRubbleVisuals(!enable); // Ensure the rubble is active when the object is inactive and vice-versa
+    }
+
+    private void ChangeRubbleVisuals(bool enable)
+    {
+        if (spawnedRubble != null)
+        {
+            // Ensure rubble is at ground level
+            var transformPosition = transform.position;
+            transformPosition.y = 0f;
+            spawnedRubble.transform.position = transformPosition;
+
+            // Disable or enable renderers
+            Renderer[] renderers = spawnedRubble.GetComponentsInChildren<Renderer>();
             foreach (Renderer renderer in renderers)
             {
                 renderer.enabled = enable;
             }
 
-            Collider[] colliders = GetComponentsInChildren<Collider>();
+            // Disable or enable colliders
+            Collider[] colliders = spawnedRubble.GetComponentsInChildren<Collider>();
             foreach (Collider collider in colliders)
             {
                 collider.enabled = enable;
             }
 
-            if (this is Chest chest)
+            // Disable or enable rigidbody physics
+            Rigidbody[] rigidbodies = spawnedRubble.GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody rigidbody in rigidbodies)
             {
-                chest.ChangeRubbleVisuals(!enable); // Ensure the rubble stays active when chest is inactive and vice-versa
+                rigidbody.isKinematic = !enable;
             }
         }
+    }
 
-        private void NotifyClientsOfDestruction(Vector3 position)
+    private void NotifyClientsOfDestruction()
+    {
+        NotifyClientsOfDestructionClientRpc();
+    }
+
+    [ClientRpc]
+    private void NotifyClientsOfDestructionClientRpc()
+    {
+        if (!IsOwner)
         {
-            if (IsOwner)
-            {
-                InformOtherClientsOfDestructionClientRpc(position);
-            }
+            HandleDestructionVisualUpdates();
         }
+    }
 
-        [ClientRpc]
-        private void InformOtherClientsOfDestructionClientRpc(Vector3 position)
-        {
-            if (!IsOwner)
-            {
-                PlayDestructionVFX(position);
-                SpawnRubble(position);
-            }
-        }
+    private void HandleDestructionVisualUpdates()
+    {
+        ChangeObjectVisuals(false);
+        PlayDestructionVFX(transform.position);
+        SpawnRubble(transform.position);
+    }
 
-        private void PlayDestructionVFX(Vector3 position)
+    protected virtual void PlayDestructionVFX(Vector3 position)
+    {
+        if (destructionVFX != null)
         {
             GameObject vfxInstance = Instantiate(destructionVFX, position, Quaternion.identity);
             var particleSystem = vfxInstance.GetComponent<ParticleSystem>();
@@ -129,10 +198,41 @@ namespace Unity.Multiplayer.Samples.SocialHub.Gameplay
                 Destroy(vfxInstance, totalWaitTime);
             }
         }
+    }
 
-        protected virtual void SpawnRubble(Vector3 position)
+    protected virtual void SpawnRubble(Vector3 position)
+    {
+        if (rubblePrefab != null && spawnedRubble == null)
         {
-            // Intended to be overridden by derived classes.
+            spawnedRubble = Instantiate(rubblePrefab, position, Quaternion.identity);
+            if (spawnedRubble.TryGetComponent<NetworkObject>(out var networkObject))
+            {
+                networkObject.Spawn(true);
+            }
+            ChangeRubbleVisuals(true);
+        }
+        else
+        {
+            ChangeRubbleVisuals(true);
         }
     }
+
+    public override void OnNetworkDespawn()
+    {
+        base.OnNetworkDespawn();
+        if (spawnedRubble != null)
+        {
+            if (spawnedRubble.TryGetComponent<NetworkObject>(out var networkObject))
+            {
+                networkObject.Despawn(true);
+            }
+            else
+            {
+                Destroy(spawnedRubble);
+            }
+            spawnedRubble = null;
+        }
+    }
+}
+
 }
