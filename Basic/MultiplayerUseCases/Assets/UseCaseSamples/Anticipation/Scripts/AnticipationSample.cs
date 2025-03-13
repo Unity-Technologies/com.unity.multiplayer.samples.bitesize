aae002f0471f1619ecca312e8d228f4fb5e52203
Paolo Abela <rikudefuffs@gmail.com>
2025-03-13 10:15:20 +0000

    chore: copied Anticipation Sample files into Multiplayer use cases'

---
+++ Basic/MultiplayerUseCases/Assets/UseCaseSamples/Anticipation/Scripts/AnticipationSample.cs
@@ -0,0 +1,313 @@
+using DefaultNamespace;
+using Unity.Collections;
+using Unity.Multiplayer.Tools.NetworkSimulator.Runtime;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AnticipationSample : NetworkBehaviour
+{
+    /// <summary>
+    /// This value is a snap value with correct anticipation. When the player changes the value, an RPC will be
+    /// sent to the server, and the server will change to the same value.
+    /// </summary>
+    public AnticipatedNetworkVariable<float> ValueA = new AnticipatedNetworkVariable<float>(0);
+    /// <summary>
+    /// This value is a snap value with incorrect anticipation. When the player changes the value, an RPC will be
+    /// sent to the server, and the server will change to a random value. The anticipation will then snap to the
+    /// new value.
+    /// </summary>
+    public AnticipatedNetworkVariable<float> ValueB = new AnticipatedNetworkVariable<float>(0);
+
+    /// <summary>
+    /// This value is a smooth value with correct anticipation. When the player changes the value, an RPC will be
+    /// sent to the server, and the server will change to the same value. The result will be the same as a snap value
+    /// on the local client, but will smooth when viewed on a remote client.
+    /// </summary>
+    public AnticipatedNetworkVariable<float> ValueC = new AnticipatedNetworkVariable<float>(0);
+    /// <summary>
+    /// This value is a smooth value with incorrect anticipation. When the player changes the value, an RPC will be
+    /// sent to the server, and the server will change to a random value. The anticipation will then interpolate to the
+    /// new value.
+    /// </summary>
+    public AnticipatedNetworkVariable<float> ValueD = new AnticipatedNetworkVariable<float>(0);
+
+    /// <summary>
+    /// This is a server-controlled value that gets updated by the server, and the client anticipates what it should be
+    /// "now" based on the latency to the server (knowing that the value it sees from the server is actually in the past)
+    ///
+    /// Smoothing is applied every frame, while the Reanticipate callback is only called when data changes, so it is
+    /// best to handle these kinds of situations via some logic in your game code rather than via jumps in the actual
+    /// value in order to maintain the most consistent player experience.
+    /// </summary>
+    public AnticipatedNetworkVariable<float> ValueE = new AnticipatedNetworkVariable<float>(0, StaleDataHandling.Reanticipate);
+
+    public NetworkManager NetworkManagerObject;
+    public PlayerMovableObject Player;
+
+    [Rpc(SendTo.Server)]
+    void SetValueARpc(float value)
+    {
+        ValueA.AuthoritativeValue = value;
+        LogEverywhereRpc($"Set value A to {ValueA.AuthoritativeValue}");
+    }
+
+    [Rpc(SendTo.Server)]
+    void SetValueBRpc(float value)
+    {
+        ValueB.AuthoritativeValue = Random.Range(0f, 10f);
+        LogEverywhereRpc($"Set value B to {ValueB.AuthoritativeValue}");
+    }
+
+    [Rpc(SendTo.Server)]
+    void SetValueCRpc(float value)
+    {
+        var previousValue = ValueC.AuthoritativeValue;
+        ValueC.AuthoritativeValue = value;
+        ValueC.Smooth(previousValue, value, SmoothTime, Mathf.Lerp);
+        LogEverywhereRpc($"Set value C to {ValueC.AuthoritativeValue}");
+    }
+
+    [Rpc(SendTo.Server)]
+    void SetValueDRpc(float value)
+    {
+        ValueD.AuthoritativeValue = Random.Range(0f, 10f);
+        LogEverywhereRpc($"Set value D to {ValueD.AuthoritativeValue}");
+    }
+
+    [Rpc(SendTo.Everyone)]
+    void LogEverywhereRpc(FixedString128Bytes message)
+    {
+        Debug.Log(message.ToString());
+    }
+
+
+    const float k_ValueEChangePerSecond = 2.5f;
+
+    public override void OnReanticipate(double lastRoundTripTime)
+    {
+        // Initialize the reanticipation for all of the values:
+        // C and D react to a request to reanticipate by simply smoothing between the previous anticipated value
+        // and the new authoritative value. They are not frequently updated and only need any reanticipation action
+        // when the anticipation was wrong.
+
+        if (ValueC.ShouldReanticipate)
+        {
+            ValueC.Smooth(ValueC.PreviousAnticipatedValue, ValueC.AuthoritativeValue, SmoothTime, Mathf.Lerp);
+        }
+        if (ValueD.ShouldReanticipate)
+        {
+            ValueD.Smooth(ValueD.PreviousAnticipatedValue, ValueD.AuthoritativeValue, SmoothTime, Mathf.Lerp);
+        }
+
+        // E is actually trying to anticipate the current value of a constantly changing object to hide latency.
+        // It uses the amount of time that has passed since the authoritativetime to gauge the latency of this update
+        // and anticipates a new value based on that delay. The server value is in the past, so the predicted value
+        // attempts to guess what the value is in the present.
+        if (ValueE.ShouldReanticipate)
+        {
+            // There is an important distinction between the smoothing this is doing and the smoothing the player object
+            // is doing:
+            // For the player object, it is replaying everything that has happened over a full round trip, so it has to
+            // account for the entire difference between the current time and the authoritative time.
+            // For this variable, we are only extrapolating over the time that has passed since the server sent us this
+            // value - the difference between current time and authoritativeTime represents a full round trip, but the
+            // actual time difference here is only a half round trip, so we multiply by 0.5.
+            // Then, because smoothing adds its own latency, we add the smooth time into the mix.
+            var secondsBehind = lastRoundTripTime * 0.5f + SmoothTime;
+
+            var newAnticipatedValue = (float)(ValueE.AuthoritativeValue + k_ValueEChangePerSecond * secondsBehind) % 10;
+
+            // This variable uses a custom interpolation callback that handles the drop from 10
+            // down to 0. Without this, there is either weird smoothing behavior, or hitching.
+            // This keeps the interpolation going, and handles the case where the interpolated value
+            // goes over 10 and has to jump back to 0.
+            ValueE.Smooth(ValueE.PreviousAnticipatedValue, newAnticipatedValue, SmoothTime, ((start, end, amount) =>
+            {
+                if (end < 3 && start > 7)
+                {
+                    end += 10;
+                }
+
+                return Mathf.Lerp(start, end, amount) % 10;
+            }));
+        };
+
+        AnticipatedNetworkVariable<float>.OnAuthoritativeValueChangedDelegate onUpdate = (AnticipatedNetworkVariable<float> variable, in float value, in float newValue) =>
+        {
+            Debug.Log($"{variable.Name} value updated to {newValue}");
+        };
+        ValueA.OnAuthoritativeValueChanged = onUpdate;
+        ValueB.OnAuthoritativeValueChanged = onUpdate;
+        ValueC.OnAuthoritativeValueChanged = onUpdate;
+        ValueD.OnAuthoritativeValueChanged = onUpdate;
+    }
+
+    [SerializeField]
+    NetworkSimulator networkSimulator;
+
+    void Update()
+    {
+        if (Restart && !NetworkManagerObject.IsListening && !NetworkManagerObject.ShutdownInProgress)
+        {
+            SetDebugSimulatorParameters(Latency, Jitter, 0);
+            NetworkManagerObject.StartClient();
+            Restart = false;
+        }
+        if (IsServer)
+        {
+            ValueE.AuthoritativeValue = (ValueE.AuthoritativeValue + k_ValueEChangePerSecond * Time.deltaTime) % 10;
+        }
+    }
+
+    int Latency = 200;
+    int Jitter = 25;
+    float SmoothTime = 0.25f;
+    bool Restart = false;
+
+    void OnGUI()
+    {
+        Vector3 scale = new Vector3(Screen.width / 910f, Screen.height / 600f, 1.0f);
+        GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, scale);
+        if (NetworkManagerObject.IsListening)
+        {
+            GUILayout.BeginArea(new Rect(0, 0, 900, 72));
+            GUILayout.Label("Anticipated Network Variable:");
+            GUILayout.Label("Each pair of sliders represents a network variable's authoritative and anticipate values. Changing the top slider sends an RPC to the server, which updates the bottom slider. The top slider shows the current 'anticipated' value, including any smoothing, while the bottom represents the authoritative value.");
+            GUILayout.EndArea();
+            GUILayout.BeginArea(new Rect(0, 72, 300, 300));
+
+            GUILayout.BeginVertical("Box");
+            GUILayout.Label("Value A (snap, correct anticipation):");
+            var updatedValue = GUILayout.HorizontalSlider(ValueA.Value, 0, 10);
+            if (updatedValue != ValueA.Value)
+            {
+                ValueA.Anticipate(updatedValue);
+                SetValueARpc(updatedValue);
+            }
+            GUILayout.Label("Value A Current Server Value:");
+            GUILayout.HorizontalSlider(ValueA.AuthoritativeValue, 0, 10);
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical("Box");
+            GUILayout.Label("Value B (snap, incorrect anticipation):");
+            updatedValue = GUILayout.HorizontalSlider(ValueB.Value, 0, 10);
+            if (updatedValue != ValueB.Value)
+            {
+                ValueB.Anticipate(updatedValue);
+                SetValueBRpc(updatedValue);
+            }
+            GUILayout.Label("Value B Current Server Value:");
+            GUILayout.HorizontalSlider(ValueB.AuthoritativeValue, 0, 10);
+            GUILayout.EndVertical();
+
+            GUILayout.EndArea();
+            GUILayout.BeginArea(new Rect(305, 72, 300, 300));
+
+            GUILayout.BeginVertical("Box");
+            GUILayout.Label("Value C (smooth, correct anticipation):");
+            updatedValue = GUILayout.HorizontalSlider(ValueC.Value, 0, 10);
+            if (updatedValue != ValueC.Value)
+            {
+                ValueC.Anticipate(updatedValue);
+                SetValueCRpc(updatedValue);
+            }
+            GUILayout.Label("Value C Current Server Value:");
+            GUILayout.HorizontalSlider(ValueC.AuthoritativeValue, 0, 10);
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical("Box");
+            GUILayout.Label("Value D (smooth, incorrect anticipation):");
+            updatedValue = GUILayout.HorizontalSlider(ValueD.Value, 0, 10);
+            if (updatedValue != ValueD.Value)
+            {
+                ValueD.Anticipate(updatedValue);
+                SetValueDRpc(updatedValue);
+            }
+            GUILayout.Label("Value D Current Server Value:");
+            GUILayout.HorizontalSlider(ValueD.AuthoritativeValue, 0, 10);
+            GUILayout.EndVertical();
+
+            GUILayout.EndArea();
+            GUILayout.BeginArea(new Rect(610, 72, 300, 300));
+
+            GUILayout.BeginVertical("Box");
+            GUILayout.Label("Value E (Server-controlled, continuous anticipation):");
+            GUILayout.HorizontalSlider(ValueE.Value, 0, 10);
+            GUILayout.Label("Value E Current Server Value:");
+            GUILayout.HorizontalSlider(ValueE.AuthoritativeValue, 0, 10);
+            GUILayout.EndVertical();
+
+            if (IsClient)
+            {
+                GUILayout.Label("");
+                GUILayout.Label($"Variable smooth duration: {SmoothTime}s");
+                SmoothTime = GUILayout.HorizontalSlider(SmoothTime, 0, 1);
+            }
+
+            GUILayout.EndArea();
+            if (IsClient)
+            {
+                GUILayout.BeginArea(new Rect(0, 310, 600, 300));
+                GUILayout.Label("Anticipated Network Transform controls:");
+                GUILayout.Label("W: Move Forward | S: Move Backward | A: Turn Left | D: Turn Right");
+                GUILayout.Label("Q: Large random teleport (very different server result)");
+                GUILayout.Label("E: Small random teleport (slightly different server result)");
+                GUILayout.Label("R: Return to center (same server result)");
+                GUILayout.Label("");
+
+                GUILayout.Label($"Transform smooth duration: {Player.SmoothTime}s");
+                Player.SmoothTime = GUILayout.HorizontalSlider(Player.SmoothTime, 0, 1);
+                GUILayout.Label($"Transform smooth distance threshold: {Player.SmoothDistance}");
+                Player.SmoothDistance = GUILayout.HorizontalSlider(Player.SmoothDistance, 0, 50);
+                if (GUILayout.Button("Toggle Server Visualization (Follower)"))
+                {
+                    foreach (var childRenderer in Player.GhostTrasform.GetComponentsInChildren<MeshRenderer>())
+                    {
+                        childRenderer.enabled = !childRenderer.enabled;
+                    }
+                }
+                GUILayout.EndArea();
+                GUILayout.BeginArea(new Rect(610, 456, 300, 150));
+                GUILayout.Label($"Latency: {Latency}ms");
+                Latency = (int)GUILayout.HorizontalSlider(Latency, 0, 300);
+                GUILayout.Label($"Jitter: {Jitter}ms");
+                Jitter = (int)GUILayout.HorizontalSlider(Jitter, 0, 50);
+                if (GUILayout.Button("Apply"))
+                {
+                    Restart = true;
+                    NetworkManagerObject.Shutdown();
+                }
+                GUILayout.EndArea();
+            }
+        }
+        else
+        {
+            GUILayout.BeginArea(new Rect(0, 0, 300, 600));
+
+            if (!NetworkManagerObject.IsListening && !Restart)
+            {
+                if (GUILayout.Button("Start Server"))
+                {
+                    SetDebugSimulatorParameters(Latency, Jitter, 0);
+                    NetworkManagerObject.StartServer();
+                }
+                if (GUILayout.Button("Start Client"))
+                {
+                    SetDebugSimulatorParameters(Latency, Jitter, 0);
+                    NetworkManagerObject.StartClient();
+                }
+            }
+            GUILayout.EndArea();
+        }
+    }
+
+    void SetDebugSimulatorParameters(int latency, int jitter, int dropRate)
+    {
+        INetworkSimulatorPreset preset = networkSimulator.CurrentPreset;
+        preset.PacketDelayMs = Latency;
+        preset.PacketJitterMs = Jitter;
+        networkSimulator.ChangeConnectionPreset(preset);
+    }
+}
---
+++ Basic/MultiplayerUseCases/Assets/UseCaseSamples/Anticipation/Scripts/InputManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace DefaultNamespace
+{
+    /// <summary>
+    /// Stores current and historical player input and allows that input to be queried.
+    /// Could be optimized to cache the input and re-read at the start of each frame instead of reading
+    /// when GetInput() is called. If this were used in more than one place, that would be necessary,
+    /// as it would otherwise result in multiple inputs being pushed into the history each frame.
+    /// </summary>
+    public class InputManager : MonoBehaviour
+    {
+        public NetworkManager NetworkManager;
+        private FrameHistory<InputList> m_HistoricalInput = new FrameHistory<InputList>();
+        private InputList m_LastInput;
+
+        /// <summary>
+        /// Retrieve input for the current frame.
+        /// </summary>
+        /// <returns></returns>
+        public InputList GetInput()
+        {
+            if (!NetworkManager.IsListening)
+            {
+                return 0;
+            }
+
+            InputList input = 0;
+            if (Input.GetKey(KeyCode.W))
+            {
+                input |= InputList.Up;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                input |= InputList.Left;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                input |= InputList.Down;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                input |= InputList.Right;
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                input |= InputList.RandomTeleport;
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                input |= InputList.SmallRandomTeleport;
+            }
+            if (Input.GetKey(KeyCode.R))
+            {
+                input |= InputList.PredictableTeleport;
+            }
+
+            // To simulate checks for GetKeyDown while in FixedUpdate:
+            // We store the unaltered input each frame. Then we alter the current frame's input
+            // so that if these buttons were pressed, we remove them from the input we are going to
+            // return and add to the history. That ensure no two input frames in a row contain these inputs,
+            // while still letting us do the input polling within FixedUpdate.
+            var lastInput = m_LastInput;
+            m_LastInput = input;
+
+            if ((lastInput & InputList.RandomTeleport) != 0)
+            {
+                input &= ~InputList.RandomTeleport;
+            }
+            if ((lastInput & InputList.SmallRandomTeleport) != 0)
+            {
+                input &= ~InputList.SmallRandomTeleport;
+            }
+            if ((lastInput & InputList.PredictableTeleport) != 0)
+            {
+                input &= ~InputList.PredictableTeleport;
+            }
+
+            m_HistoricalInput.Add(NetworkManager.LocalTime.Time, input);
+
+            return input;
+        }
+
+        /// <summary>
+        /// Remove historical input before the given time
+        /// </summary>
+        /// <param name="time"></param>
+        public void RemoveBefore(double time)
+        {
+            m_HistoricalInput.RemoveBefore(time);
+        }
+
+        /// <summary>
+        /// Retrieves historical inputs
+        /// </summary>
+        /// <returns></returns>
+        public List<FrameHistory<InputList>.ItemFrameData> GetHistory()
+        {
+            return m_HistoricalInput.GetHistory();
+        }
+
+        /// <summary>
+        /// Remove all items from the history
+        /// </summary>
+        public void Clear()
+        {
+            m_HistoricalInput.Clear();
+        }
+    }
+}
