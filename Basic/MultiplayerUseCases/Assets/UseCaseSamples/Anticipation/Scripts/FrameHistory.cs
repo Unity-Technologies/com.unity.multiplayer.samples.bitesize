7c07a52e6de5f97708f277aa900c7a962d778131
Paolo Abela <rikudefuffs@gmail.com>
2025-03-21 15:29:37 +0000

    Anticipation Sample - Unity 6 upgrade (#MTT-8661) (#273)
    * chore: updated to Unity 6 and removed unneeded packages
    
    * chore: updated NGO version to 2.2.0
    
    * chore: added multipayer tools
    
    * feat: NetworkSimulator is used instead of obsolete tech for setting Latency and Jitter
    
    * chore: replaced AnticipatedNetworkTransform component of NGO 1.8.0 with the one from 2.2.0
    
    * feat: added NetworkSimulator to the scene
    
    * chore: optimized the scene: removed unneeded shadows, skyboxes
    
    * chore: set player movement authority to "Server" instead of "Owner"
    
    * fix
    
    This resolves the issue with the U6 upgrade of the Anticipation Sample.
    
    * feat: the sample now uses the Universal Render Pipeline (URP) instead of the Built-in ones. Materials have been converted to URP materials.
    
    * feat: enabled "enter play mode settings" so that domain and scene realods no longer occur when entering play mode (saves you time)
    
    * chore: copied Anticipation Sample files into Multiplayer use cases'
    
    * chore: removed unneeded Anticipation samples file from Use Cases
    
    * chore: added EventSystem to Anticipation sample scene and regenerated guid of scene as it was conflicting
    
    * feat: removed IMGUI "Start server" and "Start Client" buttons
    
    * feat: added Anticipation-specific inputs to InputActions
    
    * chore: player of Anticipation scene is now a prefab, removed unneeded references
    
    * feat: removed player ghost from scene and made it a lazy-initilized prefab of the Player
    feat: added player to Anticipation scene's NetworkManager's prefab slot
    
    * feat: Anticipation scene in Multiplayer Use Cases uses the new input system
    
    * feat: migrated controls instructions from IMGUI to UITK
    
    * feat: (WIP) reimplemented anticipation's sample IMGUI sliders in UITK
    
    * feat: added setup utils for UITK
    
    * feat: migrated behaviour of Latency and Jitter sliders, and toggle visualization / apply buttons from IMGUI to UITK
    
    * feat: migrated Transform smoothing sliders to UITK
    
    * chore: adjusted size of UI buttons in Anticipation scene
    
    * feat: migrated Variable smoothing slider to UITK
    
    * feat: migrated VariableE UGUI to UITK
    
    * feat: migrated VariableA IMGUI sliders to UITK
    
    * feat: migrated VariableB IMGUI sliders to UITK
    
    * feat: migrated VariableC and VariableD IMGUI sliders to UITK
    
    * chore: script cleanup
    
    * feat: anticipation's sample controls only show up when the local client starts
    
    * bgufix: fixed NRE when clicking "Apply"
    
    * fix: fixed ghost transform of disconnected players not despawning
    
    * feat: added small info about Host mode in Anticipation sample
    
    * fix: fixed Anticipation's UI reacting to player inputs
    
    * feat: migrated offline UI of Anticipation scene from UGUI to UITK
    
    * chore: made Anticipation materials unlit and removed Directional Light from scene
    
    * chore: moved all inlined styles of the new Anticipation UI to USS files
    
    * fix: fixed UI disappearing when clicking "Apply"
    
    * feat: added flag to optionally ignore the "server-only overlay" in scenes, and used it in AnticipationScene.
    
    * chore: adjusted nonsense copy
    
    ---------
    
    Co-authored-by: NoelStephensUnity <noel.stephens@unity3d.com>

---
+++ Basic/MultiplayerUseCases/Assets/UseCaseSamples/Anticipation/Scripts/FrameHistory.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+
+namespace Unity.Netcode.Samples.MultiplayerUseCases.Anticipation
+{
+    /// <summary>
+    /// Simple container to store historical data associated with a frame for the sake of replaying that data.
+    /// Main use case in this demo is storing historical input for the player character so we can replay those inputs
+    /// when we need to re-anticipate a new location based on updated server data.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class FrameHistory<T>
+    {
+        public struct ItemFrameData
+        {
+            public double Time;
+            public T Item;
+        }
+        private List<ItemFrameData> m_History = new List<ItemFrameData>();
+
+        /// <summary>
+        /// Add a value to the history for the current frame.
+        /// This generally expects that items will be added to the history in the order that they occur
+        /// (i.e., each call to this has a time with a greater value than the previous). Nothing enforces this
+        /// expectation, but failure to follow it could result in things being replayed out of order later, as
+        /// there is no sorting done within this class.
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="value"></param>
+        public void Add(double time, T value)
+        {
+            m_History.Add(new ItemFrameData { Time = time, Item = value });
+        }
+
+        /// <summary>
+        /// Remove all items before a given time. Useful to keep memory usage from growing by discarding old data
+        /// that you know you won't need anymore.
+        /// </summary>
+        /// <param name="time"></param>
+        public void RemoveBefore(double time)
+        {
+            m_History.RemoveAll(item => item.Time < time);
+        }
+
+        /// <summary>
+        /// Remove all items after a given time. Useful if the data you are storing here needs to be replaced
+        /// as part of a re-anticipation action (i.e., if you are storing some historical position data and want
+        /// to recalculate everything after a given time)
+        /// </summary>
+        /// <param name="time"></param>
+        public void RemoveAfter(double time)
+        {
+            m_History.RemoveAll(item => item.Time > time);
+        }
+
+        /// <summary>
+        /// Remove all items from the history
+        /// </summary>
+        public void Clear()
+        {
+            m_History.Clear(); ;
+        }
+
+        /// <summary>
+        /// Get the full history, useful for iterating through all the values to reapply them when reanticipating.
+        /// </summary>
+        /// <returns></returns>
+        public List<ItemFrameData> GetHistory()
+        {
+            return m_History;
+        }
+    }
+}
