ca704c8b2ca0025565452b8bbdd15ef5f7b71713
Fernando Cortez <fernando.cortez@unity3d.com>
2025-04-30 09:48:54 -0400

    feat: Dedicated Game Server sample - IETs expanded [MTT-11649] (#279)
    * upgrading project to 6000.0.36f1
    
    * added multiplayer services sdk, playmode scenarios, config as code, backfill
    
    * multiplayer role usage in favour of defines
    
    * stricter roles on some scenes, null-ref cleanups
    
    * adding client matchmaking as its own state, stopping matchmaker request added
    
    * adding SQP to serverlisteningstate, matchmakerhandler added
    
    * defines guarding matchmaker code - for enabling IP editor workflow
    
    * adding IET table of contents, welcome page, 1st IET: setup UGS
    
    * adding iet settings file
    
    * importing MPPM tutorial
    
    * added content selection overview tutorial
    
    * end of MPPM tutorial text fix
    
    * PR feedback, Resources section added

--- Experimental/DedicatedGameServer/Assets/Editor/Tutorials/TutorialCallbacks.cs
+++ Experimental/DedicatedGameServer/Assets/Editor/Tutorials/TutorialCallbacks.cs
@@ -1,14 +1,11 @@
 using System;
 using System.IO;
-using Unity.Multiplayer.Tools.Editor.MultiplayerToolsWindow;
 using Newtonsoft.Json.Linq;
-using Unity.Services.Authentication;
-using Unity.Services.Core;
 using Unity.Services.Multiplayer;
 using UnityEngine;
 using UnityEditor;
 using Unity.Tutorials.Core.Editor;
-using UnityEngine.UIElements;
+using UnityEditor.SceneManagement;
 
 namespace Unity.DedicatedGameServerSample.Editor.Tutorials
 {
@@ -18,7 +15,11 @@ namespace Unity.DedicatedGameServerSample.Editor.Tutorials
     [CreateAssetMenu(fileName = k_DefaultFileName, menuName = "Tutorials/" + k_DefaultFileName + " Instance")]
     public class TutorialCallbacks : ScriptableObject
     {
-        [SerializeField] SceneAsset m_BootstrapScene;
+        [SerializeField]
+        SceneAsset m_BootstrapScene;
+
+        [SerializeField]
+        SceneAsset m_GameScene;
 
         /// <summary>
         /// The default file name used to create asset of this class type.
@@ -29,13 +30,9 @@ namespace Unity.DedicatedGameServerSample.Editor.Tutorials
 
         bool m_IsEditorWindowFocused;
 
-        const float k_QuerySessionsInterval = 5f;
-
         bool m_IsSessionCreatedByVirtualPlayer;
 
         bool m_IsSessionJoinedByEditor;
-
-        float m_TimeSinceLastSessionUpdate;
 
         ISession m_JoinedSession;
 
@@ -68,7 +65,25 @@ namespace Unity.DedicatedGameServerSample.Editor.Tutorials
 
         public void LoadBootstrapScene()
         {
-            UnityEditor.SceneManagement.EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(m_BootstrapScene));
+            EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(m_BootstrapScene));
+        }
+
+        public void LoadGameScene()
+        {
+            EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(m_GameScene));
+        }
+
+        public void SelectObjectInHierarchyByName(string objecName)
+        {
+            var obj = GameObject.Find(objecName);
+            if (obj != null)
+            {
+                Selection.activeObject = obj;
+            }
+            else
+            {
+                Debug.LogError($"Object with name {objecName} not found in scene.");
+            }
         }
 
         public bool IsConnectedToUgs()
@@ -81,13 +96,12 @@ namespace Unity.DedicatedGameServerSample.Editor.Tutorials
             SettingsService.OpenProjectSettings("Project/Services");
         }
 
-        [ContextMenu("Show Vivox Settings")]
-        public void ShowVivoxSettings()
+        public void ShowMultiplayerRolesSettings()
         {
-            SettingsService.OpenProjectSettings("Project/Services/Vivox");
+            SettingsService.OpenProjectSettings("Project/Multiplayer");
         }
 
-        public bool IsVirtualPlayerCreated()
+        public bool IsVirtualPlayer4Created()
         {
             var path = Path.Combine(Application.dataPath, k_SystemDataPath);
 
@@ -99,58 +113,12 @@ namespace Unity.DedicatedGameServerSample.Editor.Tutorials
                 var jsonObject = JObject.Parse(jsonContent);
 
                 // Access the "Data" property and then the "2" player's "Active" state
-                var isPlayer2Active = jsonObject["Data"]["2"]["Active"].Value<bool>();
+                var isPlayer4Active = jsonObject["Data"]["4"]["Active"].Value<bool>();
 
-                return isPlayer2Active;
+                return isPlayer4Active;
             }
 
             return false;
-        }
-
-        public void OnOpenMultiplayerToolsWindowTutorialStarted()
-        {
-            MultiplayerToolsWindow.Open();
-            m_IsEditorWindowFocused = false;
-        }
-
-        public bool IsSceneViewFocused()
-        {
-            if (EditorWindow.focusedWindow != null && EditorWindow.focusedWindow.titleContent.text == "Scene")
-            {
-                m_IsEditorWindowFocused = true;
-            }
-
-            return m_IsEditorWindowFocused;
-        }
-
-        VisualElement m_SceneRoot;
-
-        public void OnEnableNetSceneVisTutorialStarted()
-        {
-            m_SceneRoot = EditorWindow.GetWindow<SceneView>().rootVisualElement;
-            while (m_SceneRoot.parent != null)
-            {
-                m_SceneRoot = m_SceneRoot.parent;
-            }
-        }
-
-        public bool IsNetworkVisualizationOverlayDisplayed()
-        {
-            return m_SceneRoot != null && m_SceneRoot.Q<VisualElement>("NetVisToolbarOverlay") != null;
-        }
-
-        public void ForceNetworkVisualizationOverlayDisplayed()
-        {
-            if (m_SceneRoot.Q<VisualElement>("NetVisToolbarOverlay") == null)
-            {
-                var netSceneVis = m_SceneRoot.Q<VisualElement>("Network Visualization");
-                var netSceneVisButton = netSceneVis.Q<Button>();
-                using (var e = new NavigationSubmitEvent())
-                {
-                    e.target = netSceneVisButton;
-                    netSceneVisButton.SendEvent(e);
-                }
-            }
         }
 
         public bool IsVirtualPlayerSessionCreated()
@@ -161,84 +129,17 @@ namespace Unity.DedicatedGameServerSample.Editor.Tutorials
         public void OnCreatingSessionTutorialStarted()
         {
             m_IsSessionCreatedByVirtualPlayer = false;
-            m_TimeSinceLastSessionUpdate = Time.realtimeSinceStartup;
         }
 
-        public void QuerySessions()
+        public void OpenPrefabView(GameObject prefab)
         {
-            if (UnityServices.Instance == null || AuthenticationService.Instance == null || !AuthenticationService.Instance.IsAuthorized)
-            {
-                return;
-            }
-
-            if (Time.realtimeSinceStartup - m_TimeSinceLastSessionUpdate > k_QuerySessionsInterval)
-            {
-                m_TimeSinceLastSessionUpdate = Time.realtimeSinceStartup;
-                QuerySessionsAsync();
-            }
+            AssetDatabase.OpenAsset(prefab);
+            SceneView.FrameLastActiveSceneView();
         }
 
-        async void QuerySessionsAsync()
+        public void ExitPrefabView()
         {
-            var task = MultiplayerService.Instance.QuerySessionsAsync(new QuerySessionsOptions());
-            await task;
-            if (task.IsCompleted)
-            {
-                // todo: add more criteria here
-                m_IsSessionCreatedByVirtualPlayer = task.Result.Sessions.Count > 0;
-            }
-        }
-
-        public bool IsSessionJoinedByEditor()
-        {
-            return m_IsSessionJoinedByEditor;
-        }
-
-        public void OnJoiningSessionTutorialStarted()
-        {
-            m_IsSessionJoinedByEditor = false;
-            m_TimeSinceLastSessionUpdate = Time.realtimeSinceStartup;
-        }
-
-        public void QueryJoinedSessions()
-        {
-            if (UnityServices.Instance == null || AuthenticationService.Instance == null || !AuthenticationService.Instance.IsAuthorized)
-            {
-                return;
-            }
-
-            if (Time.realtimeSinceStartup - m_TimeSinceLastSessionUpdate > k_QuerySessionsInterval)
-            {
-                m_TimeSinceLastSessionUpdate = Time.realtimeSinceStartup;
-                QueryJoinedSessionsAsync();
-            }
-        }
-
-        async void QueryJoinedSessionsAsync()
-        {
-            var task = MultiplayerService.Instance.GetJoinedSessionIdsAsync();
-            await task;
-            if (task.IsCompleted)
-            {
-                if (task.Result.Count > 0)
-                {
-                    var joinedSessionId = task.Result[0];
-                    m_IsSessionJoinedByEditor = true;
-                    foreach (var session in MultiplayerService.Instance.Sessions)
-                    {
-                        if (session.Value.Id == joinedSessionId)
-                        {
-                            m_JoinedSession = session.Value;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        public bool IsOnlyEditorInSession()
-        {
-            return m_JoinedSession != null && m_JoinedSession.Players.Count == 1 && m_JoinedSession.IsHost;
+            StageUtility.GoToMainStage();
         }
     }
 }
